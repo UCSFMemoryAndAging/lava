5bb2d47ddce775304c29fd301a77f4741658e61d
Charlie Toohey <ctoohey@memory.ucsf.edu>
2009-04-09 19:20:31 +0000

    got rid of special-case handling for shared instrument flows, which eliminates (for now) the need to refactor this so that different scopes can have a shot at flow event handling

--- lava-core/src/edu/ucsf/lava/core/webflow/FlowListener.java
+++ lava-core/src/edu/ucsf/lava/core/webflow/FlowListener.java
@@ -5,12 +5,10 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.webflow.context.servlet.ServletExternalContext;
-import org.springframework.webflow.core.collection.AttributeMap;
 import org.springframework.webflow.core.collection.MutableAttributeMap;
 import org.springframework.webflow.definition.FlowDefinition;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.FlowExecutionListenerAdapter;
-import org.springframework.webflow.execution.FlowSession;
 import org.springframework.webflow.execution.RequestContext;
 
 import edu.ucsf.lava.core.action.ActionManager;
@@ -22,8 +20,6 @@ import edu.ucsf.lava.core.manager.ManagersAware;
 import edu.ucsf.lava.core.metadata.MetadataManager;
 import edu.ucsf.lava.core.session.CoreSessionUtils;
 import edu.ucsf.lava.core.session.SessionManager;
-import edu.ucsf.lava.crms.assessment.controller.InstrumentGroupHandler;
-
 
 
 public class FlowListener extends FlowExecutionListenerAdapter implements ManagersAware {
@@ -70,10 +66,8 @@ public class FlowListener extends FlowExecutionListenerAdapter implements Manage
 	
 //TODO: when upgrade to Spring Web Flow 2.x, change this to sessionCreating. this should solve the
 //problem where currently the flow scope already exists in sessionStarting (per the javadocs it should not)
-//and furthermore, an even bigger probleme exists, which is that if this is called when starting a child subflow, 
-//this flow scope which should not exist yet contains the attributes from the parent flow. 
-//this would only currently present a problem if an instrument CRUD flow had an instrument CRUD subflow, because 
-//the subflow would erroneously find instrTypeEncoded of the parent flow instrument in flow scope and use that. 
+//and furthermore, an even bigger potential problem exists, which is that if this is called when starting a 
+//child subflow, this flow scope which should not exist yet contains the attributes from the parent flow. 
 //since this situation does not exist in our system yet, do not need to upgrade immediately	
 	public void sessionStarting(RequestContext context, FlowDefinition definition, MutableAttributeMap input) {
 		// the current action is guaranteed to be identical for all states of a flow, since all flows are
@@ -96,29 +90,6 @@ public class FlowListener extends FlowExecutionListenerAdapter implements Manage
 		//   assumes the module/section of whichever flow is active)
 
 		this.updateCurrentAction(context, definition.getId());
-			
-		
-		// special handling beyond setting the current action for instrument flows where the target part of the 
-		// action is instrTypeEncoded which in turn is used to obtain the class of the instrument to load:
-		// instrTypeEncoded can always be obtained directly from the current action, but also have to store this 
-		// in flow scope because of an issue with this listener when returning from subflows: strangely, the resumed() 
-		// (and subsequent) listener methods are not called for a parent flow when returning from a subflow until 
-		// after any actions on the return state are invoked (but before transitions on events returned by the
-		// actions). the subflow return state invokes refreshFormObject, which tries to reload the instrument based 
-		// on the class obtained via the instrTypeEncoded from the current action, but the problem is that this 
-		// updateCurrentAction method has not been called yet, so the current action may not have the correct 
-		// instrTypeEncoded as its target part (if the subflow was for a different instrument or entity). thus, the 
-		// instrTypeEncoded must also be stored in flow scope, so that it can be obtained from there for 
-		// instrument loading in getBackingObjects, instead of from the target part of the current action
-		// note: can not simply put instrTypeEncoded in flow scope when it is determined in updateCurrentAction
-		// because that is called during sessionStarting and flow scope does not exist yet
-		HttpServletRequest request = ((ServletExternalContext)context.getExternalContext()).getRequest();
-		String actionId = CoreSessionUtils.getCurrentAction(sessionManager, request).getId();
-		if ((ActionUtils.getModule(actionId).equals("assessment") && ActionUtils.getSection(actionId).equals("instrument")) || 
-		    (ActionUtils.getModule(actionId).equals("assessment") && ActionUtils.getSection(actionId).equals("diagnosis") &&
-	    		ActionUtils.getTarget(actionId).startsWith("macdiagnosis"))) {
-			input.put("instrTypeEncoded", ActionUtils.getTarget(actionId));
-		}
 	}
 	
 	
@@ -156,79 +127,6 @@ public class FlowListener extends FlowExecutionListenerAdapter implements Manage
 		// get the default scope flow id because later it will be passed to ActionService getAction
 		// which will convert it to its project or instance scope if it should
 		String flowActionId = ActionUtils.getDefaultActionIdFromFlowId(flowId, flowIdModePart);
-		
-		// special handling for instruments which use the shared instrument flows. since these all have flow id
-		// lava.assessment.instrument.instrument.*, need to get instrTypeEncoded for use in setting current action
-		// additionally, all macdiagnosis versions share the same flows, lava.assessment.diagnosis.macdiagnosis.*,
-		// so this special handling determines the specific macdiagnosis version for the current action
-		if ((ActionUtils.getModule(flowActionId).equals("assessment") && ActionUtils.getSection(flowActionId).equals("instrument")
-				&& ActionUtils.getTarget(flowActionId).equals("instrument")) ||
-			(ActionUtils.getModule(flowActionId).equals("assessment") && ActionUtils.getSection(flowActionId).equals("diagnosis") 
-				&& ActionUtils.getTarget(flowActionId).equals("macdiagnosis"))) {
-			// for instrument entity CRUD flow definitions, the flow id name does not reflect the
-			// specific type of instrument because the flow definitions are shared among all instruments
-			// i.e. the shared flow id is 
-			// "lava.assessment.instrument.instrument.add|delete|enter|collect|view"
-			// however, the current action id must reflect the specific instrument type, so that
-			// the correct instrument specific view will be generated by CustomViewSelector.
-
-			// for the MAC Diagnosis flows, the target in the flow id will be "macdiagnosis" 
-			// e.g. "lava.assessment.diagnosis.macdiagnosis.collect"
-			// and instrTypeEncoded will be set to the target part of the request URL resulting in a version
-			// specific action, e.g. lava.assessment.diagnosis.macdiagnosis2_7_0 
-			
-			// first, attempt to get instrTypeEncoded from flow scope, because that is where it is stored
-			// when the instrument flow is first started (see sessionStarting method above). if it is not 
-			// in flow scope then the instrument flow has just been started and instrTypeEncoded can be obtained 
-			// from request URL. it is guaranteed that when an instrument CRUD flow is started initially that 
-			// the request URL will be assessment/instrument (or assessment/diagnosis)
-			
-			// note that in certain cases, e.g. when returning to the instrument CRUD flow from a subflow, 
-			// can not rely on the request URL to get instrTypeEncoded. 
-			// e.g. assessment/instrument/neuroimaging is parent flow of assessment/instrument/ni_Scan, 
-			// and when the ni_Scan view is closed, it posts the request to assessment/instrument/ni_Scan, 
-			// terminating the ni_Scan subflow, and resuming the neuroimaging parent flow. at this point the 
-			// request URL would erroneously give ni_Scan as instrTypeEncoded when it should be neuroimaging 
-			// -- while obtaining instrTypeEncoded from flow scope will correctly be neuroimaging
-			AttributeMap flowScope = context.getFlowScope();
-			String instrTypeEncoded;
-			// for new flow, flow scope will not exist yet
-			if (flowScope == null || (instrTypeEncoded = context.getFlowScope().getString("instrTypeEncoded")) == null) {
-				// instrTypeEncoded comes from the request URL when the instrument CRUD flow is launched
-				// in the usual way, e.g. from an instrument list
-				
-				// instrTypeEncoded comes from the instrumentGroup parent flow when the instrument CRUD
-				// flow is launched by the instrumentGroup flow
-				
-				FlowSession parentFlow = context.getFlowExecutionContext().getActiveSession().getParent();
-				String target = null;
-				// "sessionStarting" subflows may still refer to flow execution context of their parent
-				// flow, in which case parentFlow = null, so check for that. also, could have root flows that do
-				// not have a parent flow.
-				if (parentFlow != null) {
-					String parentFlowId = parentFlow.getDefinition().getId();
-					String actionId = ActionUtils.getActionIdFromFlowId(parentFlowId, new StringBuffer());
-					target = ActionUtils.getTarget(actionId);
-				}
-				
-				if (target != null && target.equals(InstrumentGroupHandler.TARGET)) {
-					instrTypeEncoded = context.getFlowExecutionContext().getActiveSession().getParent().getScope().getString("instrTypeEncoded");
-				}
-				else {
-					// extract the instrTypeEncoded from request URL. 
-					instrTypeEncoded = ActionUtils.getTarget(ActionUtils.getActionId(request));
-				}
-			}
-			
-			if (instrTypeEncoded.startsWith("macdiagnosis")) {
-				flowActionId = new StringBuffer(ActionUtils.LAVA_INSTANCE_IDENTIFIER).append(".crms.assessment.diagnosis.").append(instrTypeEncoded).toString();
-			}
-			else {
-				flowActionId = new StringBuffer(ActionUtils.LAVA_INSTANCE_IDENTIFIER).append(".crms.assessment.instrument.").append(instrTypeEncoded).toString();
-			}
-		}
-
-		
 		
 		// call getAction to determine whether there is an instance specific customization of the action
 		Action currentAction = actionManager.getEffectiveAction(request,  flowActionId);
