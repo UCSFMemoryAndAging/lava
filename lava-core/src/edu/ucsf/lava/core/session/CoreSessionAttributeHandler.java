3866c0c5bfef399ebc72c285ad41dfe94e329484
Charlie Toohey <ctoohey@memory.ucsf.edu>
2009-06-18 20:57:28 +0000

    fixed bug, INTER_FLOW_FORM_ERROR was not a handled attribute

--- lava-core/src/edu/ucsf/lava/core/session/CoreSessionAttributeHandler.java
+++ lava-core/src/edu/ucsf/lava/core/session/CoreSessionAttributeHandler.java
@@ -3,15 +3,13 @@ package edu.ucsf.lava.core.session;
 import static edu.ucsf.lava.core.session.CoreSessionUtils.CORE_SCOPE;
 import static edu.ucsf.lava.core.session.CoreSessionUtils.CURRENT_ACTION;
 import static edu.ucsf.lava.core.session.CoreSessionUtils.CURRENT_USER;
+import static edu.ucsf.lava.core.session.CoreSessionUtils.INTER_FLOW_FORM_ERRORS;
 
 import javax.servlet.http.HttpServletRequest;
 
 import edu.ucsf.lava.core.scope.AbstractScopeSessionAttributeHandler;
-public class CoreSessionAttributeHandler extends AbstractScopeSessionAttributeHandler {
 
-	
-	
-	
+public class CoreSessionAttributeHandler extends AbstractScopeSessionAttributeHandler {
 	
 	public CoreSessionAttributeHandler() {
 		super();
@@ -19,6 +17,7 @@ public class CoreSessionAttributeHandler extends AbstractScopeSessionAttributeHa
 		this.setHandledScope(CORE_SCOPE);
 		this.addHandledAttribute(CURRENT_USER);
 		this.addHandledAttribute(CURRENT_ACTION);
+		this.addHandledAttribute(INTER_FLOW_FORM_ERRORS);
 	}
 
 
