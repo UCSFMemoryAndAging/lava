720fb80346a8b2827dbe6789d4b3fa9b31a73665
Joe Hesse <jhesse@memory.ucsf.edu>
2009-07-06 21:03:31 +0000

    *** empty log message ***

--- lava-crms/database/crms/versions/V3.0/V3.0.3/model/lava-crms-update-3.0.2-to-3.0.3.sql
+++ lava-crms/database/crms/versions/V3.0/V3.0.3/model/lava-crms-update-3.0.2-to-3.0.3.sql
@@ -5,7 +5,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 insert into versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
 	VALUES ('lava-crms-model','3.0.3',NOW(),3,0,3,1);
 
-ALTER TABLE `lava_crms`.`patientconsent` 
+ALTER TABLE `projectunit`
  ADD INDEX `projectunit_ProjUnitDesc` (`ProjUnitDesc` ASC);
 
 ALTER TABLE `patientconsent` 
