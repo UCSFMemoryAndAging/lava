dcf60691fb7ba2812672d4b8f650e73e23775f44
Charlie Toohey <ctoohey@memory.ucsf.edu>
2010-05-04 00:28:39 +0000

    refactored exporting code into more generic (non csv specific) solution and retrieve list of instruments to export so if an instrument specific export, no longer have to parse the summary, can export whatever properties you want

--- lava-crms/src/edu/ucsf/lava/crms/assessment/controller/ProjectInstrumentsHandler.java
+++ lava-crms/src/edu/ucsf/lava/crms/assessment/controller/ProjectInstrumentsHandler.java
@@ -1,23 +1,25 @@
 package edu.ucsf.lava.crms.assessment.controller;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
-import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
 import edu.ucsf.lava.core.controller.ComponentCommand;
 import edu.ucsf.lava.core.controller.ScrollablePagedListHolder;
 import edu.ucsf.lava.core.controller.ScrollablePagedListHolder.ListItem;
 import edu.ucsf.lava.core.dao.LavaDaoFilter;
+import edu.ucsf.lava.core.dao.LavaDaoParam;
 import edu.ucsf.lava.core.dao.LavaDateRangeParamHandler;
+import edu.ucsf.lava.core.model.EntityBase;
 import edu.ucsf.lava.core.session.CoreSessionUtils;
 import edu.ucsf.lava.crms.assessment.model.Instrument;
 import edu.ucsf.lava.crms.assessment.model.InstrumentTracking;
@@ -55,44 +57,21 @@ public class ProjectInstrumentsHandler extends CrmsCalendarComponentHandler {
 		return super.addReferenceData(context, command, errors, model);
 	}
 	
-	public boolean supportsAttachedFiles() {
-		return true;
-	}
-
-	// override default implementation because not dealing with a physical file; rather, the file contents
-	// are generated and just put into the output stream
-	public Event handleDownload(RequestContext context, Object command, BindingResult errors) throws Exception {
-		Map components = ((ComponentCommand)command).getComponents();
-		HttpServletResponse response = ((ServletExternalContext)context.getExternalContext()).getResponse();
-		ScrollablePagedListHolder plh = (ScrollablePagedListHolder) components.get(this.getDefaultObjectName());
-		if (plh.getListSize() == 0) {
-//TODO: set object error msg
-			return new Event(this,ERROR_FLOW_EVENT_ID);
-		}
-
-		StringBuffer csvDefaultFilename = new StringBuffer();
-		String csvString = getCsvExport(context, command, errors, csvDefaultFilename); 
-		response.setHeader("Content-Disposition","attachment; filename=\"" + csvDefaultFilename.toString() + "\"");
-		//response.setContentType(file.getContentType());
-		response.setContentLength(csvString.length());
-		FileCopyUtils.copy(csvString.getBytes(),response.getOutputStream());
-		return new Event(this,SUCCESS_FLOW_EVENT_ID);
-	}
-	
-	
 	/**
-	 * Generate the csv export as a csv String and return it. Also, set the default csv export
-	 * filename and return via the csvDefaultFilename argument.
+	 * Generate the export as a String and return it. Also, set the default export
+	 * filename and return via the defaultFilename argument.
 	 * 
 	 * @param context
 	 * @param command
 	 * @param errors
-	 * @param csvDefaultFilename
+	 * @param defaultFilename
 	 * @return
 	 * @throws Exception
 	 */
-	protected String getCsvExport(RequestContext context, Object command, BindingResult errors, StringBuffer csvDefaultFilename) throws Exception{
+	protected String getExportContent(RequestContext context, Object command, BindingResult errors, StringBuffer defaultFilename) throws Exception{
 		HttpServletRequest request =  ((ServletExternalContext)context.getExternalContext()).getRequest();
+		
+		// for now, only .csv export is supported
 
 		// iterate thru the ScrollablePagedListHolder, creating a csv String for each record
 		ScrollablePagedListHolder plh = (ScrollablePagedListHolder) ((ComponentCommand)command).getComponents().get(this.getDefaultObjectName());
@@ -100,21 +79,27 @@ public class ProjectInstrumentsHandler extends CrmsCalendarComponentHandler {
 		List<InstrumentTracking> instrList = plh.getSourceAsEntityList();
 		List list = plh.getSource();
 		
-		// when the filter is used to specify an instrument type, it is possible that an instrument 
-		// specific export could be done, e.g. with instrument specific default export filename,
-		// instrument specific column headers (beyond the standard column headers), and instrument
-		// specific parsing of the instrument summary to generate a .csv with individual summary values
+		// when the Filter is used to specify an instrument type, such that all of the instruments in
+		// the list are the same (or related) it is possible that an instrument specific export could 
+		// be done, with instrument specific default export filename, instrument specific column headers 
+		// (beyond the standard column headers), and instrument specific summary properties
+
+		// if an instrument specific export can not be done, a generic export is done, exporting the 
+		// summary property for each instrument, along with the common properties from InstrumentTracking
 		
-		// because the source list is of InstrumentTracking objects, they can not be used for
-		// this instrument specific functionality. however, since the functionality would be the same
-		// for all instruments of a given type, just load the first instrument in the list and use it 
-		// for this instrument specific functionality
+		
+		// rather than iterating thru the List<InstrumentTracking> and checking each item to determine
+		// if they are all of same type (which could require additional retrieval for list items that
+		// have not been loaded yet), easier just to see if the first item in the list matches the
+		// Filter "Instrument Type". instruments define a method to determine this and will only
+		// return true if the "Instrument Type" is specific enough to denote just that type of
+		// instrument (and possibly aliases)
 		
 		// note: already know the list is not empty, or this method would not have been called
 		boolean instrSpecificExport = false;
 		Instrument instrSpecificInstrument = null;
  		String filterInstrType = (String) filter.getParams().get("instrType");
- 		if (!StringUtils.isEmpty(filterInstrType)) {
+ 		if (!org.apache.commons.lang.StringUtils.isEmpty(filterInstrType)) {
  			LavaDaoFilter instrFilter = InstrumentTracking.newFilterInstance(CoreSessionUtils.getCurrentUser(this.sessionManager, request));
  			Instrument instrument = (Instrument) ((ListItem) list.iterator().next()).getEntity();
 			instrFilter.addIdDaoEqualityParam(instrument.getId());
@@ -124,57 +109,56 @@ public class ProjectInstrumentsHandler extends CrmsCalendarComponentHandler {
  			}
  		}
  		
- 		//TODO: if instrSpecificInstrument is true, instead of calling getCsvSummaryData to parse the
- 		// individual data values for that instrument out of the summary, just retrieve a new instrument list 
- 		// of objects of that instrument type (subject to other user filter settings). this will allow
- 		// us to export values that are not part of the summary and not have to parse. additionally, will
- 		// not need to continually call doNext when run out of loaded items 
-		
- 		
- 		// default csv filename
+ 		// default export filename
  		if (instrSpecificExport) {
- 			csvDefaultFilename.append(instrSpecificInstrument.getCsvListDefaultFilename());
+ 			defaultFilename.append(instrSpecificInstrument.getExportListDefaultFilename()).append(".csv");
  		}
  		else {
  			// use the instrument tracking object
- 			csvDefaultFilename.append(((Instrument)((ListItem) list.iterator().next()).getEntity()).getCsvListDefaultFilename());
+ 			defaultFilename.append(((Instrument)((ListItem) list.iterator().next()).getEntity()).getExportListDefaultFilename()).append(".csv");
  		}
  		
+
  		StringBuffer csvString = new StringBuffer();
+ 		
 		// column headers
- 		csvString.append(((Instrument)((ListItem) list.iterator().next()).getEntity()).getCsvCommonColumnHeaders());
+ 		csvString.append(StringUtils.arrayToCommaDelimitedString(((Instrument)((ListItem) list.iterator().next()).getEntity()).getExportCommonColHeaders()));
  		if (instrSpecificExport) {
- 			csvString.append(instrSpecificInstrument.getCsvSummaryColumnHeaders());
+ 			csvString.append(",").append(StringUtils.arrayToCommaDelimitedString(instrSpecificInstrument.getExportSummaryColHeaders()));
  		}
  		else {
- 			csvString.append(((Instrument)((ListItem) list.iterator().next()).getEntity()).getCsvSummaryColumnHeaders());
+ 			csvString.append(",").append(StringUtils.arrayToCommaDelimitedString(((Instrument)((ListItem) list.iterator().next()).getEntity()).getExportSummaryColHeaders()));
  		}
  		csvString.append("\n");
- 		
-		// data
- 		for(Object listItem : list){
- 			// when get to entities that have not been loaded yet (due to ScrollablePagedListHolder
- 			// efficiency), issue next page commands until they are loaded (may take several of these,
- 			// e.g. given 473 items, first 100 are loaded, 25 per page
- 			// when get to item 101, the entity = null, so need to load more
- 			// the first call to doNextPage will check if items 26 thru 50 are loaded, and they are, so 
- 			// nothing happens, the second call checks 51 thru 75, and so on, so it will not be until
- 			// the fourth call that the next items beyond 100 are loaded
- 			while (((ListItem)listItem).getEntity() == null) {
- 	 			this.doNextPage(context, command, errors);
- 			}
- 			
- 			InstrumentTracking instrument = (InstrumentTracking) ((ListItem)listItem).getEntity();
- 			csvString.append(instrument.getCsvCommonData());
- 			
- 	 		if (instrSpecificExport) {
- 	 			csvString.append(instrSpecificInstrument.getCsvSummaryData(instrument));
- 	 		}
- 	 		else {
-	 			csvString.append(((Instrument)((ListItem) list.iterator().next()).getEntity()).getCsvSummaryData(instrument));
- 	 		}
-	 		csvString.append("\n");
+		
+ 		// column data
+ 		List<Instrument> fullInstrList;
+		LavaDaoFilter instrFilter = EntityBase.newFilterInstance(getCurrentUser(request));
+		instrFilter = prepareFilter(context,instrFilter,((ComponentCommand)command).getComponents());
+		// transfer the sorts and daoParams over to get user settings
+		for (LavaDaoParam daoParam : filter.getDaoParams()) {
+			instrFilter.addDaoParam(daoParam);
+		}
+		for (Iterator iter = filter.getSort().entrySet().iterator(); iter.hasNext(); ) {
+			Entry entry = (Entry) iter.next();
+			instrFilter.addSort((String)entry.getKey(), ((Boolean)entry.getValue()).booleanValue());
+		}
+		// obtain a full list of either the instrument specfic instrument objects, or if the list is
+		// not instrument specific, and list of InstrumentTracking objects (while ScrollablePagedListHolder
+		// already has a List<InstrumentTracking> it may not be completely populated since only the first
+		// N list objects are retrieved for performance purposes)
+ 		if (instrSpecificExport) {
+ 			fullInstrList = Instrument.MANAGER.get(instrSpecificInstrument.getClass(), instrFilter);
  		}
+ 		else {
+ 			fullInstrList = Instrument.MANAGER.get(InstrumentTracking.class, instrFilter);
+ 		}
+ 		// for each instrument, append a CSV record
+		for (Instrument instr : fullInstrList) {
+ 			csvString.append(StringUtils.arrayToCommaDelimitedString(instr.getExportCommonData()));
+ 			csvString.append(",").append(StringUtils.arrayToCommaDelimitedString(instr.getExportSummaryData()));
+	 		csvString.append("\n");
+		}
 
  		return csvString.toString();
 	}
