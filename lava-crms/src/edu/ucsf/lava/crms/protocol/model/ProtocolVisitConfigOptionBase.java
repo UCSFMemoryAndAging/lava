54c68ffe1d63362087d645c32b8dfe575d90ac68
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-01-20 23:02:25 +0000

    renamed from ProtocolVisitOptionConfigBase

---
+++ lava-crms/src/edu/ucsf/lava/crms/protocol/model/ProtocolVisitConfigOptionBase.java
@@ -0,0 +1,31 @@
+package edu.ucsf.lava.crms.protocol.model;
+
+import edu.ucsf.lava.core.model.EntityBase;
+import edu.ucsf.lava.core.model.EntityManager;
+
+/**
+ * This class exists to allow protocol tree retrieval queries which only involving the
+ * protocol option config base table. This class is mapped such that it is not a subclass of 
+ * the protocol option config base table.
+ * 
+ * @author ctoohey
+ * 
+ *
+ */
+
+public class ProtocolVisitConfigOptionBase extends ProtocolConfigOptionBase {
+	public ProtocolVisitConfigOptionBase(){
+		super();
+		this.addPropertyToAuditIgnoreList("protocolVisitConfigBase");
+	}
+
+	private ProtocolVisitConfigBase protocolVisitConfigBase;
+
+	public ProtocolVisitConfigBase getProtocolVisitConfigBase() {
+		return protocolVisitConfigBase;
+	}
+
+	public void setProtocolVisitConfigBase(ProtocolVisitConfigBase protocolVisitConfig) {
+		this.protocolVisitConfigBase = protocolVisitConfig;
+	}
+}
