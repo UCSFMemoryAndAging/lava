9d2a44028e550bf52b3f82ea6b6595e02f0273df
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-03-15 19:32:48 +0000

    emory feature: in conjunction with changes in CrmsAuthUser, allow users to specify a startupProject as a preference

--- lava-crms/src/edu/ucsf/lava/crms/auth/CrmsAuthUserContextFilter.java
+++ lava-crms/src/edu/ucsf/lava/crms/auth/CrmsAuthUserContextFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import edu.ucsf.lava.core.home.model.PreferenceUtils;
 import edu.ucsf.lava.core.manager.CoreManagerUtils;
 import edu.ucsf.lava.core.manager.Managers;
 import edu.ucsf.lava.core.manager.ManagersAware;
@@ -31,11 +32,33 @@ public class CrmsAuthUserContextFilter implements Filter, ManagersAware {
 		
 		CrmsAuthUser user = CrmsSessionUtils.getCrmsCurrentUser(sessionManager, httpServletRequest);
 		
-		if(user!=null && user.isAuthUserContextInit()){
+		if(user!=null && user.isAuthUserContextInit()) {
 			List<String> userProjectAccess = user.getProjectAccessList();
-			if(userProjectAccess.size() == 1 && ! user.getAllProjectAccess() && !userProjectAccess.get(0).equals(CrmsAuthUser.PROJECT_LIST_PLACEHOLDER)){
-				CrmsSessionUtils.setCurrentProject(sessionManager,httpServletRequest, userProjectAccess.get(0));
-				logger.info("set CurrentProject=" + userProjectAccess.get(0));
+			String newProjectContext = "";
+			boolean newProjectContextSpecified = false;
+			
+			// initialize CrmsAuthUser context if haven't done already
+			if(user!=null && !user.isCrmsAuthUserContextInit()) {
+				// if startupProject preference found, then set projectChangeTo to this
+				if(userProjectAccess.size() > 0) {
+					String startupProject = PreferenceUtils.getPrefValue(user, "crmsAuthUser", "startupProject", null);
+					// note: we allow the user to ensure their project context is always empty (""), even if they only have one project
+					if (startupProject!=null && (startupProject.equals("") || userProjectAccess.contains(startupProject))) {
+						newProjectContext = startupProject;
+						newProjectContextSpecified = true;
+					}
+				}
+				user.setCrmsAuthUserContextInit(true);
+			}
+			
+			if(newProjectContextSpecified==false && userProjectAccess.size() == 1 && ! user.getAllProjectAccess() && !userProjectAccess.get(0).equals(CrmsAuthUser.PROJECT_LIST_PLACEHOLDER)) {
+				newProjectContext = userProjectAccess.get(0);
+				newProjectContextSpecified = true;
+			}
+			
+			if (newProjectContextSpecified) {
+				CrmsSessionUtils.setCurrentProject(sessionManager,httpServletRequest, newProjectContext);
+				logger.info("set CurrentProject=" + newProjectContext);
 			}
 		}
 		chain.doFilter(request, response);
