082e844c945ffdcbbd3a0ca8d6d477d4f0ee3b37
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-05-08 20:15:46 +0000

    children is now a LinkedHashSet instead of Treeset, as ordering is done in database rather than implementing Comparable

--- lava-crms/src/edu/ucsf/lava/crms/protocol/model/ProtocolTracking.java
+++ lava-crms/src/edu/ucsf/lava/crms/protocol/model/ProtocolTracking.java
@@ -1,7 +1,7 @@
 package edu.ucsf.lava.crms.protocol.model;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 import edu.ucsf.lava.core.model.EntityBase;
 import edu.ucsf.lava.core.model.EntityManager;
@@ -17,7 +17,7 @@ import edu.ucsf.lava.core.model.EntityManager;
  *
  */
 
-public class ProtocolTracking extends ProtocolNode {
+public class ProtocolTracking extends ProtocolNode /*implements Comparable<ProtocolTracking> */ {
 	public static EntityManager MANAGER = new EntityBase.Manager(ProtocolTracking.class);
 	
 	public ProtocolTracking(){
@@ -28,7 +28,7 @@ public class ProtocolTracking extends ProtocolNode {
 	private ProtocolTracking parent;
 	private ProtocolConfigTracking config; 
 	// use TreeSet so collection can be sorted chronologically in subclasses, if desired
-	private Set<ProtocolTracking> children = new TreeSet<ProtocolTracking>();
+	private Set<ProtocolTracking> children = new LinkedHashSet<ProtocolTracking>();
 
 	public ProtocolTracking getParent() {
 		return parent;
@@ -72,4 +72,71 @@ public class ProtocolTracking extends ProtocolNode {
 		
 	}
 	
+	
+	/**
+	 * The ProtocolTimepoint collection should be ordered chronologically. If the patient has started
+	 * the protocol, i.e. a Visit has been assigned as the primary visit of the first timepoint, then
+	 * the schedWinAnchorDate can be calculated for every timepoint and since this is a persisted
+	 * property the collection could be retrieved in this order. However, if the patient has not started
+	 * the protocol, schedWinAnchorDate will be null. Still want to have an ordered collection though,
+	 * for proper display of the protocol. So must instead order the timepoints collection based on the 
+	 * protocol configuration. In particular, order on the ProtocolTimepointConfig listOrder 
+	 * property of the timepoint's corresponding protocol timepoint configuration. In order to do this,
+	 * must implement the compareTo method.    
+	 * 
+	 * Note that while this sorts ProtocolTracking objects of all types, only TIMEPOINT instances
+	 * will have the schedWinAnchorDate and/or listOrder properties populated, so essentially this is
+	 * a timepoint sort.
+	 */
+	public int compareTo(ProtocolTracking protocolTracking) throws ClassCastException {
+		// first try to order on schedWinAnchorDate
+		if (this.getSchedWinAnchorDate() != null && protocolTracking.getSchedWinAnchorDate() != null) {
+	        if (this.getSchedWinAnchorDate().after(protocolTracking.getSchedWinAnchorDate()))
+	            return 1;
+	        else if (this.getSchedWinAnchorDate().before(protocolTracking.getSchedWinAnchorDate()))
+	            return -1;
+	        else
+	            return 0;
+		}
+		else {
+			if (this.getConfig().getListOrder() == null 
+					&& protocolTracking.getConfig().getListOrder() == null) {
+				return 0;
+			}
+			else if (this.getConfig().getListOrder() != null 
+					&& protocolTracking.getConfig().getListOrder() == null) {
+				return 1;
+			}
+			else if (this.getConfig().getListOrder() == null 
+					&& protocolTracking.getConfig().getListOrder() != null) {
+				return -1;
+			}
+			
+	        if (this.getConfig().getListOrder() 
+	        		> protocolTracking.getConfig().getListOrder())
+	            return 1;
+	        else if (this.getConfig().getListOrder() 
+	        		< protocolTracking.getConfig().getListOrder())
+	            return -1;
+	        else {
+	        	if (this.getConfig().getRepeating() != null && this.getConfig().getRepeating() 
+	        			&& protocolTracking.getConfig().getRepeating() != null && protocolTracking.getConfig().getRepeating()) {
+	        		if (this.getRepeatNum() > protocolTracking.getRepeatNum()) {
+	        			return 1;
+	        		}
+	        		else if (this.getRepeatNum() < protocolTracking.getRepeatNum()) {
+	        			return -1;
+	        		}
+	        		else { 
+	        			// should never get here, i.e. repeating timepoints with same repeatNum 
+	        			return 0; 
+	        		}
+	        	}
+	        	else {
+	        		return 0;
+	        	}
+	        }
+		}
+	}
+
 }
