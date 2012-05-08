59559289e53d41ceb1ea091e7ebd4d15f3e48110
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-05-08 00:14:25 +0000

    initial revision

---
+++ lava-crms/src/edu/ucsf/lava/crms/protocol/controller/ProjectDeviationStatusHandler.java
@@ -0,0 +1,26 @@
+package edu.ucsf.lava.crms.protocol.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.execution.RequestContext;
+
+import edu.ucsf.lava.core.dao.LavaDaoFilter;
+import edu.ucsf.lava.crms.session.CrmsSessionUtils;
+
+public class ProjectDeviationStatusHandler extends BaseDeviationStatusHandler {
+
+	public ProjectDeviationStatusHandler() {
+		super();
+		this.setHandledList("projectDeviationStatus","protocolStatusList");
+	}
+	
+	public LavaDaoFilter extractFilterFromRequest(RequestContext context, Map components) {
+		LavaDaoFilter filter = super.extractFilterFromRequest(context, components);
+		HttpServletRequest request =  ((ServletExternalContext)context.getExternalContext()).getRequest();
+		return CrmsSessionUtils.setFilterProjectContext(sessionManager,request,filter);
+	}
+	
+}
