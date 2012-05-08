1d8c034bc59198941ab89aa092ca20f7b0a56d6c
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-05-08 20:24:51 +0000

    1) optional refactored to base class so it is part of config tracking object 2) improved implementation of isPrimaryProtocolVisitConfig 3) set summary 4) got rid of defaultOption as options keep track of default on themself 5) beforeCreate assigns nodeType

--- lava-crms/src/edu/ucsf/lava/crms/protocol/model/ProtocolVisitConfig.java
+++ lava-crms/src/edu/ucsf/lava/crms/protocol/model/ProtocolVisitConfig.java
@@ -29,21 +29,13 @@ public class ProtocolVisitConfig extends ProtocolVisitConfigBase {
 	}
 	
 	public Object[] getAssociationsToInitialize(String method) {
+		// options are eagerly fetched
 		return new Object[]{
 				this.getProtocolInstrumentConfigsBase(), 
-				this.getProtocolVisitConfigOptionsBase(), 
 				this.getProtocolTimepointConfigBase().getProtocolVisitConfigsBase()};
 	}
 	
-	private Boolean optional;
 	private String category;
-	private ProtocolVisitConfigOption defaultOption;
-	// defaultOptionId facilitates modifying the defaultOption association. the user selects a 
-	// defaultOption (ProtocolVisitOptionConfig) and that defaultOptionId is bound to this property. 
-	// if it differs from this entity's existing defaultOption id, the defaultOptionId is used to 
-	// retrieve its defaultOption which is then set on this entity via setDefaultOption to change 
-	// the associated defaultOption
-	private Long defaultOptionId;
 	
 	/**
 	 * Convenience methods to convert ProtocolVisitConfigBase method types to types of 
@@ -76,19 +68,37 @@ public class ProtocolVisitConfig extends ProtocolVisitConfigBase {
 		this.getProtocolInstrumentConfigs().add(instrumentConfig);
 	}
 	
+
 	/**
-	 * Convenience method to check if this is the primary ProtocolVisitConfig of the ProtocolTimepointConfig, given
-	 * that there can be multiple visit configs. An association to the primary visit config is stored in the 
-	 * ProtocolTimepointConfig parent of the ProtocolVisitConfig collection (as opposed to having a flag on each 
-	 * ProtocolVisitConfig to designate itself as the primary, which would require synchronization among all 
-	 * sibling ProtocolVisitConfigs)
+	 * Convenience method to check if this is the primary ProtocolVisitConfig. An association to the primary
+	 * VisitConfig is stored in the ProtocolTimepointConfig parent of the ProtocolVisitConfig collection (as 
+	 * ooposed to having a flag on each ProtocolVisitConfig to designate itself as the primary, which would 
+	 * require synchronization among all sibling ProtocolVisitConfigs)
+	 * 
+	 * note: using the id property here instead of getPrimaryProtocolVisitConfig.getId() means do not
+	 * have to eagerly load the first ProtocolVisitConfig
 	 * 
 	 * @return true if this is the primary visit config, false if not.
 	 */
 	public Boolean isPrimaryProtocolVisitConfig() {
-		return this.getProtocolTimepointConfig().getPrimaryProtocolVisitConfigId().equals(this.getId());
+		// if adding, no id since it has not been saved yet
+		if (this.getId() == null) {
+			// if there is not a primary yet, then this will be the first, as the business rule is that
+			// if there is only one visit config it must be marked as the primary
+			if (this.getProtocolTimepointConfig().getPrimaryProtocolVisitConfigId() == null) {
+				return Boolean.TRUE;
+			}
+			else {
+				return Boolean.FALSE;
+			}
+		}
+		else {
+			// existing ProtocolVisitConfig, so can check the ProtocolTimepointConfig to see if this is
+			// marked as the primary
+			return this.getProtocolTimepointConfig().getPrimaryProtocolVisitConfigId().equals(this.getId());
+		}
 	}
-
+	
 	
 	/*
 	 * Method to add a ProtocolVisitConfigOption to a ProtocolVisit's collection, managing the bi-directional relationship.
@@ -98,13 +108,6 @@ public class ProtocolVisitConfig extends ProtocolVisitConfigBase {
 		this.getOptions().add(protocolVisitConfigOption);
 	}
 	
-	public Boolean getOptional() {
-		return optional;
-	}
-	public void setOptional(Boolean optional) {
-		this.optional = optional;
-	}
-
 	public String getCategory() {
 		return category;
 	}
@@ -113,38 +116,50 @@ public class ProtocolVisitConfig extends ProtocolVisitConfigBase {
 		this.category = category;
 	}
 
-	public ProtocolVisitConfigOption getDefaultOption() {
-		return defaultOption;
+	
+	protected void updateSummary() {
+		StringBuffer block = new StringBuffer();
+		if (isPrimaryProtocolVisitConfig()) {
+			block.append("Primary Visit");
+		}
+		if (this.getOptional()) {
+			block.append(" (optional)");
+		}
+		
+		this.setSummary(block.toString());
 	}
-
-	public void setDefaultOption(ProtocolVisitConfigOption defaultOption) {
-		this.defaultOption = defaultOption;
-		if (this.defaultOption != null) {
-			this.defaultOptionId = this.defaultOption.getId();
+	
+	
+	public void updateCalculatedFields(){
+		super.updateCalculatedFields();
+		
+		this.updateSummary();
+		
+		for (ProtocolVisitConfigOption visitConfigOption : this.getOptions()) {
+			visitConfigOption.updateSummary();
 		}
 	}
-
-	public Long getDefaultOptionId() {
-		return defaultOptionId;
-	}
-
-	public void setDefaultOptionId(Long defaultOptionId) {
-		this.defaultOptionId = defaultOptionId;
-	}
-
 	
+	
+	public void beforeCreate() {
+		super.beforeCreate();
+		this.setNodeType(VISIT_NODE);
+	}
+
+
 	public boolean afterCreate() {
 		// flag as the primary visit config if this is the only visit config
-		if (this.getProtocolTimepointConfig().getProtocolVisitConfigs().size() == 1) {
+		// note: have to do in afterCreate because need the id of the newly created entity
+		if (this.getProtocolTimepointConfig().getPrimaryProtocolVisitConfigId() == null) {
 			this.getProtocolTimepointConfig().setPrimaryProtocolVisitConfig(this);
 			// this is special case as the only place that primaryProtocolVisitConfig gets set on 
 			// ProtocolTimepointConfig other than its own handler. 
 			// have to save ProtocolTimepointConfig here
 			this.getProtocolTimepointConfig().save();
 		}
-		
-		// always save again, as node value has been set
-		return true;
+
+		// not need to save this entity and parent saved explicitly above
+		return false;
 	}
 
 	
