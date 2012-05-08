de330c56b0ff7d4879cf8cb0f9ddc566b32a4f2c
Charlie Toohey <ctoohey@memory.ucsf.edu>
2012-05-08 00:26:32 +0000

    1) mods to creating eligible instrument list in reference data. ordered by proximity to the ideal date. 2) subflow return hook now returns the entity, not the entity id. more efficient as do not have to then query for the instrument given the id. 3) doSaveAdd, doSave methods to accommodate changing the selected instrument 4) doSave now updates windows/statuses in case assigned instrument changed

--- lava-crms/src/edu/ucsf/lava/crms/protocol/controller/ProtocolInstrumentHandler.java
+++ lava-crms/src/edu/ucsf/lava/crms/protocol/controller/ProtocolInstrumentHandler.java
@@ -16,9 +16,12 @@ import org.springframework.webflow.execution.RequestContext;
 import edu.ucsf.lava.core.action.ActionUtils;
 import edu.ucsf.lava.core.controller.ComponentCommand;
 import edu.ucsf.lava.core.controller.LavaComponentFormAction;
+import edu.ucsf.lava.core.dao.LavaDaoFilter;
+import edu.ucsf.lava.core.model.EntityBase;
 import edu.ucsf.lava.crms.assessment.model.Instrument;
 import edu.ucsf.lava.crms.assessment.model.InstrumentTracking;
 import edu.ucsf.lava.crms.controller.CrmsEntityComponentHandler;
+import edu.ucsf.lava.crms.protocol.model.Protocol;
 import edu.ucsf.lava.crms.protocol.model.ProtocolInstrument;
 import edu.ucsf.lava.crms.protocol.model.ProtocolInstrumentConfigOption;
 
@@ -47,26 +50,12 @@ public class ProtocolInstrumentHandler extends CrmsEntityComponentHandler {
 		// order the list by proximity to the instrument's collection window
 		Map allMatchingInstrs = new LinkedHashMap();
 
-//TODO: just use instrument options directly in jsp		
-		// create a list of instrTypes that can fulfill the instrument requirement. put the default
-		// instrType at the beginning of the list
-		List<String> instrTypes = new ArrayList<String>();
-		for (ProtocolInstrumentConfigOption option : protocolInstrument.getProtocolInstrumentConfig().getOptions()) {
-			//TODO: ck effDate,expDate and if not currently effective, ignore						
-			//TODO: defaultOption not implemented yet
-			//if (option.getDefaultOption()) {
-			//	instrTypes.add(0, option.getInstrType());
-			//}
-			//else {
-				instrTypes.add(option.getInstrType());
-			//}
-		}
-		// put in model for informational display purposes
-		model.put("instrConfigOptions", instrTypes);
+		// create a list of instrument options that can fulfill the instrument requirement for the view to display
+		List<String> options = new ArrayList<String>();
 		
 		// generate a list of instruments for each instrType, ordered by data collection date proximity to the 
 		// ProtocolInstrumentConfig's data collection window
-		Date proximityDate = protocolInstrument.getCollectAnchorDate();
+		Date proximityDate = protocolInstrument.getInstrCollectWinAnchorDate();
 		// if a Visit has not been assigned such that the collection window and anchor date can not be calculated
 		// yet, use the current date so the result is ordered by the most recently collected instruments
 		if (proximityDate == null) {
@@ -81,22 +70,32 @@ public class ProtocolInstrumentHandler extends CrmsEntityComponentHandler {
 		// to least desired (and from above, within a given option, matches will be ordered by proximity)
 		for (ProtocolInstrumentConfigOption option : protocolInstrument.getProtocolInstrumentConfig().getOptions()) {
 			Map matchingInstrs = listManager.getDynamicList(getCurrentUser(request), "protocol.matchingInstruments", 
-					new String[]{"patientId", "instrType", "collectAnchorDate", "effDate", "expDate"},
+					new String[]{"patientId", "instrType", "collectWinAnchorDate", "effDate", "expDate"},
 					new Object[]{protocolInstrument.getPatient().getId(), 
 						option.getInstrType(), 
-						proximityDate, option.getEffectiveEffDate(), option.getEffectiveExpDate()}, 
+						proximityDate, 
+						option.getEffectiveEffDate() == null ? new Date(Long.MIN_VALUE) : option.getEffectiveEffDate(),
+						option.getEffectiveExpDate() == null ? new Date(Long.MAX_VALUE) : option.getEffectiveExpDate()}, 
 					new Class[]{Long.class, String.class, Date.class, Date.class, Date.class});	
 //ASSUME WILL NEED TO REMOVE THE BLANK ENTRY FROM LISTS AFTER THE FIRST ONE IF MULTIPLE OPTIONS						
 		
 			allMatchingInstrs.putAll(matchingInstrs);
+			
+			// construct a list of options for the view to informing the user what is valid to aid them
+			// if adding a new Instrument to assign 
+			StringBuffer sb = new StringBuffer(option.getInstrType());
+			sb.append(" (eff:").
+			append(option.getEffectiveEffDate() == null ? " " : option.getEffectiveEffDate()).
+			append(" exp:").
+			append(option.getEffectiveExpDate() == null ? " " : option.getEffectiveExpDate()).append(")");
+			options.add(sb.toString());
 		}
+
 		dynamicLists.put("protocol.matchingInstruments", allMatchingInstrs);
-		
 		model.put("dynamicLists", dynamicLists);
-		
+		model.put("instrConfigOptions", options);
 		// so protocolConfig label can appear on page title 
 		model.put("protocolConfigLabel", protocolInstrument.getProtocolVisit().getProtocolTimepoint().getProtocol().getProtocolConfig().getLabel());
-		
 		return super.addReferenceData(context, command, errors, model);
 	}
 	
@@ -124,16 +123,6 @@ public class ProtocolInstrumentHandler extends CrmsEntityComponentHandler {
 	}
 	
 	
-	protected Event doSaveAdd(RequestContext context, Object command, BindingResult errors) throws Exception{
-		this.handleInstrumentChange(context, command, errors);
-		return super.doSaveAdd(context, command, errors);
-	}
-	
-	protected Event doSave(RequestContext context, Object command, BindingResult errors) throws Exception{
-		this.handleInstrumentChange(context, command, errors);
-		return super.doSaveAdd(context, command, errors);
-	}
-	
 	/**
 	 * Override as this method is invoked when returning from a subflow, and for the Add Instrument subflow, need to
 	 * validate and add the newly created instrument.
@@ -141,9 +130,6 @@ public class ProtocolInstrumentHandler extends CrmsEntityComponentHandler {
 	public void subFlowReturnHook(RequestContext context, Object command, BindingResult errors) throws Exception {
 		ProtocolInstrument protocolInstrument = (ProtocolInstrument)((ComponentCommand)command).getComponents().get(getDefaultObjectName());
 
-		// do not call super.subFlowReturnHook as do not want to refresh this upon return from the Add Instrument subflow. 
-		// that would result in losing any pending modifications the user may have made.
-		
 		// the Edit ProtocolInstrument flow is the parent flow and spawns the Add Instrument subflow. because the user 
 		// may have made edits when they click Add Instrument, the edits are neither saved or cancelled; rather they 
 		// remain in the command object in the Edit ProtocolInstrument flow's flow scope, and when the Add Instrument
@@ -157,33 +143,25 @@ public class ProtocolInstrumentHandler extends CrmsEntityComponentHandler {
 		//instrTypes to add to the list of ProtocolInstrumentOptionConfig options. could do both of these by passing params
 		//to the Add Instrument flow and modify InstrumentHandler to process these (in same way, could restrict Add Visit
 		//visitTypes to those defined in its options)
+		//however, until this is done, validate the InstrType after the fact below
 		
 		String subflowActionId = context.getFlowScope().getString("subflowActionId");
 		// if subflowActionId not defined, must be returning from a subflow that does not map it back, e.g. 
 		// protocolInstrument edit returning to protocolInstrument view. this can be ignored, as the only subflow
 		// return to process is Add Visit and that will define subflowActionId
 		if (subflowActionId != null && ActionUtils.getSection(subflowActionId).equals("instrument")) {
-			Long subflowEntityId = context.getFlowScope().getLong("subflowEntityId");
-			if (subflowEntityId != null) { // if user cancelled the Add Visit there will be no subflowEntityId is flowScope
-				// instruments have to be retrieved by type, so even if know the id, can not retrieve it without the proper type,
-				// so have to get an InstrumentTracking object for the instrument, and then get the instrument 
-				InstrumentTracking instrTracking = (InstrumentTracking) InstrumentTracking.MANAGER.getById(subflowEntityId);
-				Class instrClass = instrumentManager.getInstrumentClass(instrTracking.getInstrTypeEncoded());
-				Instrument instr = (Instrument) Instrument.MANAGER.getById(instrClass, subflowEntityId);
-				
-				//TODO: possibly: pass the allowable InstrType to Add Instrument so it will restrict the Add
-				//to those, guaranteeing that the added Instrument fulfills the Protocol Instruemnt Configuration
-				//however, until that is done, validate the InstrType after the fact
+			Instrument assignedInstr = (Instrument) context.getFlowScope().get("subflowEntity");
+			if (assignedInstr != null) { // if user cancelled the Add Visit there will be no subflowEntityId is flowScope
 				
 				// validate the instrument type against the instrument types configured as options for this ProtocolInstrument
 				boolean typeMatch = false, dateMatch = false;
 				ProtocolInstrumentConfigOption optionMatched = null; // used for error messaging
 				for (ProtocolInstrumentConfigOption option : protocolInstrument.getProtocolInstrumentConfig().getOptions()) {
-					if (instr.getInstrType().equals(option.getInstrType())) {
+					if (assignedInstr.getInstrType().equals(option.getInstrType())) {
 						typeMatch = true;
 						// now check whether the instrument is within the effective window for the option
-						if ((option.getEffectiveEffDate() == null || !instr.getDcDate().before(option.getEffectiveEffDate())) 
-								&& (option.getEffectiveExpDate() == null || !instr.getDcDate().after(option.getEffectiveExpDate()))) {
+						if ((option.getEffectiveEffDate() == null || !assignedInstr.getDcDate().before(option.getEffectiveEffDate())) 
+								&& (option.getEffectiveExpDate() == null || !assignedInstr.getDcDate().after(option.getEffectiveExpDate()))) {
 							optionMatched = option;
 							dateMatch = true;
 							break;
@@ -195,17 +173,17 @@ public class ProtocolInstrumentHandler extends CrmsEntityComponentHandler {
 					for (ProtocolInstrumentConfigOption option : protocolInstrument.getProtocolInstrumentConfig().getOptions()) {
 						instrTypesSb.append("  [").append(option.getInstrType()).append("]");
 					}
-					LavaComponentFormAction.createCommandError(errors, "protocol.instrTypeMismatch", new String[]{instr.getInstrType(), instrTypesSb.toString()});
+					LavaComponentFormAction.createCommandError(errors, "protocol.instrTypeMismatch", new String[]{assignedInstr.getInstrType(), instrTypesSb.toString()});
 				}
 				else if (!dateMatch) {
-					LavaComponentFormAction.createCommandError(errors, "protocol.instrDateMismatch", new Object[]{instr.getInstrType(), instr.getDcDate(), optionMatched.getEffectiveEffDate(), optionMatched.getEffectiveExpDate()});
+					LavaComponentFormAction.createCommandError(errors, "protocol.instrDateMismatch", new Object[]{assignedInstr.getInstrType(), assignedInstr.getDcDate(), optionMatched.getEffectiveEffDate(), optionMatched.getEffectiveExpDate()});
 				}
 				else {
 					// only set the id here, not the Instrument object itself, as this needs to simulate user select of an Instrument. if call
 					// setInstrument here, the instrId and instrument.getId will be the same so on save it will think nothing has changed and
 					// the new instrument will not be assigned
-					protocolInstrument.setInstrId(instr.getId());
-					LavaComponentFormAction.createCommandError(errors, "info.protocol.instrAssigned", new String[]{instr.getInstrType()});
+					protocolInstrument.setInstrId(assignedInstr.getId());
+					LavaComponentFormAction.createCommandError(errors, "info.protocol.instrAssigned", new String[]{assignedInstr.getInstrType()});
 				}
 			}
 		}
@@ -214,5 +192,28 @@ public class ProtocolInstrumentHandler extends CrmsEntityComponentHandler {
 		}
 	}
 	
+
+	protected Event doSaveAdd(RequestContext context, Object command, BindingResult errors) throws Exception{
+		this.handleInstrumentChange(context, command, errors);
+		return super.doSaveAdd(context, command, errors);
+	}
 	
+	protected Event doSave(RequestContext context, Object command, BindingResult errors) throws Exception{
+		this.handleInstrumentChange(context, command, errors);
+		Event returnEvent = super.doSaveAdd(context, command, errors);
+		
+		// in case the Instrument assignment has has been modified in any way, need to recalculate windows, statuses
+		ProtocolInstrument protocolInstrument = (ProtocolInstrument)((ComponentCommand)command).getComponents().get(getDefaultObjectName());
+		LavaDaoFilter filter = ProtocolInstrument.newFilterInstance();
+		filter.addDaoParam(filter.daoNamedParam("protocolId", protocolInstrument.getProtocolVisit().getProtocolTimepoint().getProtocol().getId()));
+		Protocol protocolTree = (Protocol) EntityBase.MANAGER.findOneByNamedQuery("protocol.completeProtocolTree", filter);
+		protocolTree.calculate();
+		protocolTree.updateStatus();
+		// save from the Protocol down and hen return false so will not re-save from ProtocolVisit on down
+		protocolTree.save();
+		
+		return returnEvent;
+	}
+	
+
 }
