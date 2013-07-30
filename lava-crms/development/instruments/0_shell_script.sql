40f2c0d281048cc3fb152204bbdee55e1a452e03
Charlie Toohey <ctoohey@memory.ucsf.edu>
2013-07-30 22:49:36 +0000

    not running any database stuff as root anymore (bad practice)

--- lava-crms/development/instruments/0_shell_script.sql
+++ lava-crms/development/instruments/0_shell_script.sql
@@ -1,7 +1,7 @@
-mysql -s -r -u root -p < 2_gen_table.sql > gen_table.sql
-mysql -s -r -u root -p < gen_table.sql
-mysql -s -r -u root -p < 3_insert_metadata.sql
-mysql -s -r -u root -p < 4_gen_code.sql > gen_code.sql
-mysql -s -r -u root -p < 5_gen_metadata.sql > gen_metadata.sql
-mysql -s -r -u root -p < 6_gen_lavaquery.sql > gen_lavaquery.sql
-mysql -s -r -u root -p < gen_lavaquery.sql
+mysql -s -r -u DB_USER -p < 2_gen_table.sql > gen_table.sql
+mysql -s -r -u DB_USER -p < gen_table.sql
+mysql -s -r -u DB_USER -p < 3_insert_metadata.sql
+mysql -s -r -u DB_USER -p < 4_gen_code.sql > gen_code.sql
+mysql -s -r -u DB_USER -p < 5_gen_metadata.sql > gen_metadata.sql
+mysql -s -r -u DB_USER -p < 6_gen_lavaquery.sql > gen_lavaquery.sql
+mysql -s -r -u DB_USER -p < gen_lavaquery.sql
