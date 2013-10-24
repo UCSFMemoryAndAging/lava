87f97a3e04cdb28920dc142c8e1499d1cbe7bac5
Albert Lee <alee@memory.ucsf.edu>
2013-10-24 20:09:44 +0000

    include new `legacy_column` field for datadictionary table

--- lava-crms/database/crms/versions/V3.3/V3.3.3/update/lava-crms-model-3.3.2-to-3.3.3.sql
+++ lava-crms/database/crms/versions/V3.3/V3.3.3/update/lava-crms-model-3.3.2-to-3.3.3.sql
@@ -1,10 +1,9 @@
--- ************************************************************
--- UCSF: Add `notes` field to datadictionary table
--- ************************************************************
+-- *********************************************************************
+-- UCSF: Add `notes` and `legacy_column` fields to datadictionary table
+-- *********************************************************************
 
 ALTER TABLE `datadictionary` ADD COLUMN `notes` varchar(500) NULL DEFAULT NULL AFTER `db_default` ;
-
-
+ALTER TABLE `datadictionary` ADD COLUMN `legacy_column` varchar(50) NULL DEFAULT NULL AFTER `notes` ;
 
 insert into versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
 	VALUES ('lava-crms-model','3.3.3',NOW(),3,3,3,0);
