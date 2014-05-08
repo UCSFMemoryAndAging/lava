e617c64793d3e3ed5c77926f635ffa59a6c87a08
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-05-08 14:42:31 -0700

    continued development

--- lava-core/src/edu/ucsf/lava/core/importer/model/ImportDefinition.java
+++ lava-core/src/edu/ucsf/lava/core/importer/model/ImportDefinition.java
@@ -1,6 +1,5 @@
 package edu.ucsf.lava.core.importer.model;
 
-import org.springframework.web.multipart.MultipartFile;
 import edu.ucsf.lava.core.file.model.LavaFile;
 import edu.ucsf.lava.core.model.EntityBase;
 import edu.ucsf.lava.core.model.EntityManager;
@@ -10,26 +9,7 @@ public class ImportDefinition extends EntityBase {
 	public static EntityManager MANAGER = new EntityBase.Manager(ImportDefinition.class);
 
 	private String name;
-//?? could add: private String category; in case get so many import defs could add a sort/filter
-// by category to make it easy to find. but what would categories be? if left this freeform then 
-// doubtful there would be orgznied categories so not much help. so maybe put the field in but
-// don't use it for now
-	private String category;
-	
-// should not need if a) subclass repo and override createFileId to generate the mappingFilePath and
-// b) persist the mappingFilePath
-//	private String mappingFilename;
-	// this is the fileIdPropertyName of the repository configuration, used to store where the file is located
-	// in the repository
-	// this field is not persisted. the value is computed where the path is the PIDN and instrID and the
-	// value of repository getFileId which 
-	// LavaFileDao getFileId gets the repository and calls repository getFileId with entity as argument, but in
-	// the case of CBT the fileId is derived from PIDN and InstrID so CbtFileDao overrides getFileId and
-	// simply returns entity.getFilename, so our derived FileManager class may need to do this as well. not sure
-	// because why doesn't PdfTrackingFileDao do this as well even though it also stores PDFs under a PIDN
-	// and possibly other directory. Do these persist this field ???
-	// see where CBT derives this in CalculateController but also need to look to see where CBT derives this
-	// when storing the file in the repository
+	private String category; // not currently used. available for future use.
 	
 	private LavaFile mappingFile;
 	
@@ -37,13 +17,6 @@ public class ImportDefinition extends EntityBase {
 	//it automatically by reading first line of data file and search for tabs or commas (but 
 	//then, could have commas within the data which causes problems, so probably yes on a 
 	//format property
-	
-	
-// create the CrmsImportDefinition (should this be the CoreImportDefinition? not per Auth naming
-	// optional Project property which would be useful for filtering lists of both ImportLog and 
-	// ImportDefinition 
-	// would then have to figure out whether could authorize via Project if present and not if 
-	// not present
 	
 	private String notes;
 
@@ -78,7 +51,13 @@ public class ImportDefinition extends EntityBase {
 	public String getNotes() {
 		return notes;
 	}
+	
 	public void setNotes(String notes) {
 		this.notes = notes;
 	}
+
+	public static ImportDefinition findOneById(Long definitionId){
+		return (ImportDefinition) MANAGER.getById(ImportDefinition.class, definitionId);
+	}
+	
 }
