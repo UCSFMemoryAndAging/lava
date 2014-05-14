28df41979d9c2c94b118bab06fece136fbba30dd
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-05-13 23:57:03 -0700

    fixed up requiredFieldEvents, requiredFields importDefinition.definition list implement getLavaFileBackingObject began implementation of doImport method

--- lava-core/src/edu/ucsf/lava/core/importer/controller/ImportHandler.java
+++ lava-core/src/edu/ucsf/lava/core/importer/controller/ImportHandler.java
@@ -1,8 +1,12 @@
 package edu.ucsf.lava.core.importer.controller;
 
+import static edu.ucsf.lava.core.file.ImportRepositoryStrategy.IMPORT_DATA_FILE_TYPE;
+import static edu.ucsf.lava.core.file.ImportRepositoryStrategy.IMPORT_REPOSITORY_ID;
+
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -10,6 +14,7 @@ import java.util.Scanner;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.validation.BindingResult;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
 import org.springframework.webflow.definition.StateDefinition;
 import org.springframework.webflow.execution.Event;
@@ -21,12 +26,14 @@ import edu.ucsf.lava.core.auth.CoreAuthorizationContext;
 import edu.ucsf.lava.core.auth.model.AuthUser;
 import edu.ucsf.lava.core.controller.BaseEntityComponentHandler;
 import edu.ucsf.lava.core.controller.ComponentCommand;
+import edu.ucsf.lava.core.controller.LavaComponentFormAction;
 import edu.ucsf.lava.core.file.model.LavaFile;
 import edu.ucsf.lava.core.importer.model.ImportDefinition;
 import edu.ucsf.lava.core.importer.model.ImportLog;
 import edu.ucsf.lava.core.importer.model.ImportSetup;
 import edu.ucsf.lava.core.model.EntityBase;
 import edu.ucsf.lava.core.session.CoreSessionUtils;
+import edu.ucsf.lava.core.type.LavaDateUtils;
 
 // subclass BaseEntityComponentHandler even though there is not entity CRUD involved with importing, because
 // there are still a number of methods that are useful and applicable, such as initBinder, which therefore do
@@ -36,7 +43,7 @@ public class ImportHandler extends BaseEntityComponentHandler {
 	public ImportHandler() {
 		super();
 
-		// the following will set up "importSetup" as the defaultObjectName which is used for setting
+		// the following will set up "import" as the defaultObjectName which is used for setting
 		// the view component mode and view. the objectMap is also used to determine which objects should
 		// be acted on by the persistence methods; while "import" is not a persistent object, there
 		// are no persistence events handled by this handler so no issues having it in objectMap
@@ -53,11 +60,8 @@ public class ImportHandler extends BaseEntityComponentHandler {
 //TODO: figure out authEvents
 		authEvents = new ArrayList();
 		
-//NEED TO SET "import" as requiredFieldEvent so required field validation will be done
-		
-		this.setRequiredFields(new String[]{
-				"definitionName",
-				"dataFileInput"});
+		this.requiredFieldEvents.addAll(Arrays.asList("import"));
+		this.setRequiredFields(new String[]{"definitionId"});
 	}
 	
 	public Event authorizationCheck(RequestContext context) throws Exception {
@@ -92,13 +96,6 @@ public class ImportHandler extends BaseEntityComponentHandler {
 		return backingObjects;
 	}
 
-	protected String[] defineRequiredFields(RequestContext context, Object command) {
-// if get the MultipartFile binding working such that importFile is a property, not sure if could put it here
-	    setRequiredFields(new String[]{"definitionName"});
-	    return getRequiredFields();
-	}
-	
-	
 	public Map addReferenceData(RequestContext context, Object command, BindingResult errors, Map model)
 	{
 		HttpServletRequest request =  ((ServletExternalContext)context.getExternalContext()).getRequest();
@@ -117,8 +114,8 @@ public class ImportHandler extends BaseEntityComponentHandler {
 			this.addListsToModel(model, listManager.getStaticListsForEntity("import"));
 			
 			//	load up dynamic lists
-			Map<String,String> definitionList = listManager.getDynamicList("import.definitions"); 
-			dynamicLists.put("import.definitions", definitionList);
+			Map<String,String> definitionList = listManager.getDynamicList("importDefinition.definitions"); 
+			dynamicLists.put("importDefinition.definitions", definitionList);
 		}
 
 	 	// get the list of all importLogs for the selected import definition, to be displayed as a 
@@ -153,46 +150,84 @@ public class ImportHandler extends BaseEntityComponentHandler {
 	public Event handleImportEvent(RequestContext context, Object command, BindingResult errors) throws Exception {
 		return doImport(context, command,errors);
 	}
+	
+	
+	// this is called in getUploadFile which uploads the specified file and populates the mappingFile LavaFile properties
+	protected LavaFile getLavaFileBackingObject(RequestContext context, Map components, BindingResult errors) throws Exception{
+		HttpServletRequest request =  ((ServletExternalContext)context.getExternalContext()).getRequest();
+		LavaFile dataFile = new LavaFile();
+		dataFile.setContentType(IMPORT_DATA_FILE_TYPE);
+		dataFile.setRepositoryId(IMPORT_REPOSITORY_ID);
+		AuthUser user = CoreSessionUtils.getCurrentUser(sessionManager, request);
+		dataFile.setFileStatusBy(user.getShortUserNameRev());
+		dataFile.setFileStatusDate(LavaDateUtils.getDatePart(new Date()));
+		return dataFile;
+		/*
+		ImportSetup importSetup = (ImportSetup) components.get(getDefaultObjectName());
+		if(LavaFile.class.isAssignableFrom(importSetup.getDataFile().getClass())){
+			return (LavaFile)importSetup.getDataFile();
+		}
+		return null;
+		 * 
+		 */
+	}
+	
 		
 	protected Event doImport(RequestContext context, Object command, BindingResult errors) throws Exception {
 		ImportSetup importSetup = (ImportSetup) ((ComponentCommand)command).getComponents().get(getDefaultObjectName());
-		Long definitionId = importSetup.getDefinitionId();
-		ImportDefinition importDefinition = ImportDefinition.findOneById(definitionId);
-		LavaFile mappingFile = importDefinition.getMappingFile();
-		Scanner fileScanner = new Scanner(new ByteArrayInputStream(mappingFile.getContent()), "UTF-8");
 		
-		// line 1 is properties, line 2 is column names
-		int lineIndex = 0;
-		while (fileScanner.hasNextLine()) {
-
-			String currentLine = fileScanner.nextLine().trim();
-			lineIndex++;
-//TODO: need file format separator(comma or tab) in ImportDefinition			
-//			Scanner lineScanner = new Scanner(currentLine).useDelimiter("\t");
-			Scanner lineScanner = new Scanner(currentLine).useDelimiter(",");
-
-			while (lineScanner.hasNext()) {
-				String nextField = lineScanner.next();
-				if (lineIndex == 1) {
-					// set property in property array
-					
-				}
-				else if (lineIndex == 2) {
-					// set column in column array
-					
-				}
-			}
+		// definition and mapping file
+		importSetup.setImportDefinition(ImportDefinition.findOneById(importSetup.getDefinitionId()));
+		LavaFile mappingFile = importSetup.getImportDefinition().getMappingFile();
+		
+		// data file
+		MultipartFile dataMultipartFile = context.getRequestParameters().getMultipartFile(getDefaultObjectName() + "_uploadFile");
+		if (dataMultipartFile == null || dataMultipartFile.getSize() == 0) {
+			//TODO: error msg that no data file specified
+			LavaComponentFormAction.createCommandError(errors, "No Data File Specified");
+			return new Event(this,this.ERROR_FLOW_EVENT_ID);
 		}
 		
+		// definition mapping and data arrays
+//TODO: redo these comments with a definition for each array		
+		// dataColumns are the column headers in the data file so are specific to the data file and can be anything. the same column
+		// headers are in the definition mapping file and these are stored in columns array. the properties array is mapped to columns
+		// in the definition mapping file and the values are class property names that can be set via reflection
+	
+		// note that the data file column headers match up with the definition mapping file property names, in terms of array indices,
+		// so that do not actually need the data file column headers in the mapping file for import purposes, but they are there
+		// for clarity and guidance in creating the definition mapping file. plus it provides a validation step to compare the data
+		// file column names with the definition mapping file column names to make sure that the import is as intended
 		
-		// check that MultipartFile was supplied
-		
-		// for the specified definitionName, read the definition mapping file into a Map (or two Maps; one with column names
-		// as keys, one with property names as keys)
-		// UPDATE: may not need a Map
-		
-		// read the data file column headers
-		// validate against definition mapping file, i.e. column headers match and in same order
+		Scanner fileScanner = new Scanner(new ByteArrayInputStream(mappingFile.getContent()), "UTF-8");
+		String currentLine; 
+		// line 1 is columns, line 2 is properties
+		if (fileScanner.hasNextLine()) {
+			currentLine = fileScanner.nextLine().trim();
+//TODO: need file format separator(comma or tab) in ImportDefinition			
+			importSetup.setMappingCols(currentLine.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+		}
+		else {
+			//TODO: error msg: definition mapping file does not contain the first row of column names
+			return new Event(this,this.ERROR_FLOW_EVENT_ID);
+		}
+		if (fileScanner.hasNextLine()) {
+			currentLine = fileScanner.nextLine().trim();
+//TODO: need file format separator(comma or tab) in ImportDefinition			
+			importSetup.setMappingProps(currentLine.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+		}
+		else {
+			//TODO: error msg: definition mapping file does not contain the second row of property names
+			return new Event(this,this.ERROR_FLOW_EVENT_ID);
+		}
+
+
+//			Scanner lineScanner = new Scanner(currentLine).useDelimiter("\t");
+//			Scanner lineScanner = new Scanner(currentLine).useDelimiter(",");
+
+//			while (lineScanner.hasNext()) {
+//				String nextField = lineScanner.next();
+//			}
 		
 		
 		// create an array of entity.property to set for each CSV index
@@ -223,8 +258,6 @@ public class ImportHandler extends BaseEntityComponentHandler {
 		// note: the CrmsImportLog will have Crms specific log fields so the Crms jsp will be used
 		
 //OTHER:		
-		// choose a definition
-		// choose a data file
 		// on import:
 			// read the definition mapping file into a Map (data file column keys to entity.property values)
 			// read the data file column headers, iterating thru the definition Mapping file to find each
