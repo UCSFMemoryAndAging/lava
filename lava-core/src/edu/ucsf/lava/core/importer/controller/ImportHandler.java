d277679b714d53f03d84c4130172c7e4b065250c
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-07-08 15:09:16 -0700

    setup BeanUtils converters for String to Date conversion
    handle CSV for TAB-separated formats
    
    read in data column headers in this class instead of in crms subclass
    
    call validateDataFile method as part of doImport and implement the core
    version of that method

--- lava-core/src/edu/ucsf/lava/core/importer/controller/ImportHandler.java
+++ lava-core/src/edu/ucsf/lava/core/importer/controller/ImportHandler.java
@@ -10,9 +10,12 @@ import java.util.Scanner;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.commons.beanutils.converters.DateConverter;
 
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
@@ -34,8 +37,7 @@ import edu.ucsf.lava.core.importer.model.ImportSetup;
 import edu.ucsf.lava.core.model.EntityBase;
 import edu.ucsf.lava.core.session.CoreSessionUtils;
 import edu.ucsf.lava.core.type.LavaDateUtils;
-import static edu.ucsf.lava.core.importer.model.ImportDefinition.DEFAULT_DATE_FORMAT;
-import static edu.ucsf.lava.core.importer.model.ImportDefinition.DEFAULT_TIME_FORMAT;
+import static edu.ucsf.lava.core.importer.model.ImportDefinition.*;
 import static edu.ucsf.lava.core.file.ImportRepositoryStrategy.IMPORT_DATA_FILE_TYPE;
 import static edu.ucsf.lava.core.file.ImportRepositoryStrategy.IMPORT_REPOSITORY_ID;
 
@@ -44,6 +46,7 @@ import static edu.ucsf.lava.core.file.ImportRepositoryStrategy.IMPORT_REPOSITORY
 // there are still a number of methods that are useful and applicable, such as initBinder, which therefore do
 // not have to be implemented
 public class ImportHandler extends BaseEntityComponentHandler {
+	private ConvertUtilsBean convertUtilsBean;
 
 	public ImportHandler() {
 		super();
@@ -67,12 +70,11 @@ public class ImportHandler extends BaseEntityComponentHandler {
 		
 		this.requiredFieldEvents.addAll(Arrays.asList("import"));
 		this.setRequiredFields(new String[]{"definitionId"});
-		
-		DateConverter dateConverter = new DateConverter(null);
-		dateConverter.setPatterns(new String[]{DEFAULT_DATE_FORMAT});
-		ConvertUtils.register(dateConverter, java.util.Date.class);
+
+		this.convertUtilsBean = BeanUtilsBean.getInstance().getConvertUtils();
+		this.setupBeanUtilConverters();
 	}
-	
+
 	public Event authorizationCheck(RequestContext context) throws Exception {
 		HttpServletRequest request = ((ServletExternalContext)context.getExternalContext()).getRequest();
 		Action action = CoreSessionUtils.getCurrentAction(sessionManager, request);
@@ -89,7 +91,7 @@ public class ImportHandler extends BaseEntityComponentHandler {
 	
 	
 	/**
-	 * Create the filter used for the report query.   
+	 * Create the setup object used to do the import, as well as the log object to log import messages.   
 	 */
 	public Map getBackingObjects(RequestContext context, Map components) {
 		HttpServletRequest request =  ((ServletExternalContext)context.getExternalContext()).getRequest();
@@ -171,14 +173,6 @@ public class ImportHandler extends BaseEntityComponentHandler {
 		dataFile.setFileStatusBy(user.getShortUserNameRev());
 		dataFile.setFileStatusDate(LavaDateUtils.getDatePart(new Date()));
 		return dataFile;
-		/*
-		ImportSetup importSetup = (ImportSetup) components.get(getDefaultObjectName());
-		if(LavaFile.class.isAssignableFrom(importSetup.getDataFile().getClass())){
-			return (LavaFile)importSetup.getDataFile();
-		}
-		return null;
-		 * 
-		 */
 	}
 	
 		
@@ -197,6 +191,14 @@ public class ImportHandler extends BaseEntityComponentHandler {
 			return new Event(this,this.ERROR_FLOW_EVENT_ID);
 		}
 		
+//TODO: add import event to audit events (persisted import data file in combination with importLog will essentially be the 
+// import data audit log)		
+		
+//TODO: write the data file to the repository, regardless of what errors might follow
+		//data files need to be associated with importLog record for the import and the importLog lists
+		//should have a download action icon to allow downloading the data file that was imported
+		//UPDATE: write/call method to instantiate a CrmsImportLog, instantiate a LavaFile for the data
+		
 		// definition mapping and data arrays
 //TODO: redo these comments with a definition for each array		
 		// dataColumns are the column headers in the data file so are specific to the data file and can be anything. the same column
@@ -211,82 +213,94 @@ public class ImportHandler extends BaseEntityComponentHandler {
 		Scanner fileScanner = new Scanner(new ByteArrayInputStream(mappingFile.getContent()), "UTF-8");
 		String currentLine; 
 		// line 1 is columns, line 2 is properties
+		
+		// mapping file columns
 		if (fileScanner.hasNextLine()) {
 			currentLine = fileScanner.nextLine().trim();
-//TODO: need file format separator(comma or tab) in ImportDefinition			
-			importSetup.setMappingCols(currentLine.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+			if (importSetup.getImportDefinition().getDataFileFormat().equals(CSV_FORMAT)) {
+				//importSetup.setMappingCols(currentLine.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+				// (?:(?<=")([^"]*)(?="))|(?<=,|^)([^,]*)(?=,|$)
+				importSetup.setMappingCols(StringUtils.commaDelimitedListToStringArray(currentLine));
+			}
+			else if (importSetup.getImportDefinition().getDataFileFormat().equals(TAB_FORMAT)) {
+				//TODO: tab delimited has not been tested
+				//importSetup.setMappingCols(currentLine.split("\t(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+				importSetup.setMappingCols(StringUtils.delimitedListToStringArray(currentLine, "\t"));
+			}
 		}
 		else {
-			//TODO: error msg: definition mapping file does not contain the first row of column names
+			LavaComponentFormAction.createCommandError(errors, "Cannot import. Definition mapping file does not contain the first row of column names");
 			return new Event(this,this.ERROR_FLOW_EVENT_ID);
 		}
+		
+		// mapping file properties
 		if (fileScanner.hasNextLine()) {
 			currentLine = fileScanner.nextLine().trim();
-//TODO: need file format separator(comma or tab) in ImportDefinition			
-			importSetup.setMappingProps(currentLine.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+			if (importSetup.getImportDefinition().getDataFileFormat().equals(CSV_FORMAT)) {
+//TODO: use opencsv				
+				//importSetup.setMappingProps(currentLine.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+				//importSetup.setMappingProps(StringUtils.commaDelimitedListToStringArray(currentLine));
+				importSetup.setMappingProps(currentLine.split(",", -1));
+			}
+			else if (importSetup.getImportDefinition().getDataFileFormat().equals(TAB_FORMAT)) {
+				//TODO: tab delimited has not been tested
+				//importSetup.setMappingProps(currentLine.split("\t(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+				importSetup.setMappingProps(StringUtils.delimitedListToStringArray(currentLine, "\t"));
+			}
 		}
 		else {
-			//TODO: error msg: definition mapping file does not contain the second row of property names
+			LavaComponentFormAction.createCommandError(errors, "Cannot import. Definition mapping file does not contain the second row of property names");
 			return new Event(this,this.ERROR_FLOW_EVENT_ID);
 		}
 
+		// read in the data column headers
+		LavaFile dataFile = this.getUploadFile(context, ((ComponentCommand)command).getComponents(), errors);
+		fileScanner = new Scanner(new ByteArrayInputStream(dataFile.getContent()), "UTF-8");
+		if (fileScanner.hasNextLine()) {
+			currentLine = fileScanner.nextLine().trim();
+			if (importSetup.getImportDefinition().getDataFileFormat().equals(CSV_FORMAT)) {
+				//importSetup.setDataCols(currentLine.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+				importSetup.setDataCols(StringUtils.commaDelimitedListToStringArray(currentLine));
+			}
+			else if (importSetup.getImportDefinition().getDataFileFormat().equals(TAB_FORMAT)) {
+				//TODO: tab delimited files have not been tested
+				//importSetup.setDataCols(currentLine.split("\t(?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+				importSetup.setDataCols(StringUtils.delimitedListToStringArray(currentLine, "\t"));
+			}
+		}
 
-//			Scanner lineScanner = new Scanner(currentLine).useDelimiter("\t");
-//			Scanner lineScanner = new Scanner(currentLine).useDelimiter(",");
+		if (validateDataFile(errors, importSetup.getImportDefinition(), importSetup).getId().equals(ERROR_FLOW_EVENT_ID)) {
+			return new Event(this, ERROR_FLOW_EVENT_ID);
+		}
 
-//			while (lineScanner.hasNext()) {
-//				String nextField = lineScanner.next();
-//			}
-		
-		
-		// create an array of entity.property to set for each CSV index
-		
-		// somehow using the entity name to get the entity class, instantiate an instance of the class
-		
-		// iterate thru the data file
-		
-		// should have a helper method here which Crms can subclass to do the Crms specific parts of the 
-		// upload concerning Patient, Project, Visit
-		
-		// for each row read there will be an array of the data values
-		// using the prepared array of the entity.property names, use reflection to set each data value on the
-		// entity, i.e. array indices of entity.property names and data file values match up
-		
-		// record successful and failed records in the importLog
-		// record any warnings/alerts/info msgs in the importLog
-		
-		//TODO: validation
-		
-		// upon successful completion write the data file to the repository
-		// upon success or failure, persist the importLog
-		
-		// upon returning SUCCESS Event the flow will transition to "importLog" state which will
-		// be the same jsp (crms/importer/import/import.jsp) but will display completely different
-		// fields (could either display the importLog properties requiring metadata or could output
-		// data string)
-		// note: the CrmsImportLog will have Crms specific log fields so the Crms jsp will be used
-		
-//OTHER:		
-		// on import:
-			// read the definition mapping file into a Map (data file column keys to entity.property values)
-			// read the data file column headers, iterating thru the definition Mapping file to find each
-			//  column in the mapping file and store the CSV index (instead of current technique of having
-			//  a var for each column index, could have Map fpr each entity mapping propety to the CSV index
-			//  and since the # entity's and enitty types will be variable across all definitions could 
-			//  dynamically create a Map of these entity Maps
-		
-			// upon reading a row of data, process Patient Map first then Caregiver, Contact Info and other
-			// Patient module entities, then Visit, then assessments
-			// read row of data into CSV array and then use the indexes from the Entity map to get the
-			// value for a given property and use reflection to set the value on the property
-		
-			// TODO will be a validation step to query the metadata for an entity.property to validate
-		 	// against a range/dropdown, etc.
-		
-		
-		return new Event(this,SUCCESS_FLOW_EVENT_ID);
+		return new Event(this, SUCCESS_FLOW_EVENT_ID);		
 	}
 
+	
+	/**
+	 * Subclasses should override if there is additional validation that should take place before being reading 
+	 * and importing data. 
+	 * 
+	 * @param importDefinition
+	 * @param importSetup
+	 * @param errors
+	 * @return
+	 */
+	protected Event validateDataFile(BindingResult errors, ImportDefinition importDefinition, ImportSetup importSetup) throws Exception {
+		// validate the mapping file data columns against the import file data columns as they should be
+		// identical (mapping file is created by pasting data column headers into row 1)
+		if (importSetup.getMappingCols().length != importSetup.getDataCols().length) {
+			LavaComponentFormAction.createCommandError(errors, "Cannot import. Mismatch in number of columns in mapping file vs data file");
+			return new Event(this,ERROR_FLOW_EVENT_ID);
+		}
+		for (int i=0; i < importSetup.getMappingCols().length; i++) {
+			if (!importSetup.getMappingCols()[i].equals(importSetup.getDataCols()[i])) {
+				LavaComponentFormAction.createCommandError(errors, "Cannot import. Mapping file column name " + importSetup.getMappingCols()[i] + " does not exactly match column header in data file");
+				return new Event(this,ERROR_FLOW_EVENT_ID);
+			}
+		}
+		return new Event(this, SUCCESS_FLOW_EVENT_ID);
+	}
 	
 
 	public Event handleCloseEvent(RequestContext context, Object command, BindingResult errors) throws Exception {
@@ -299,6 +313,27 @@ public class ImportHandler extends BaseEntityComponentHandler {
 	}
 
 
+	protected void setupBeanUtilConverters() {
+		// default to throwing exceptions on conversion errors (this is the default anyway, but explicitly
+		// set here in case handling was temporarily changed
+		this.getConvertUtilsBean().register(true, true, -1);
+		
+		// register a DateConverter to handle String to java.util.Date conversion
+		DateConverter dateConverter = new DateConverter(null);
+		// add any additional date formats to be converted to this array
+		dateConverter.setPatterns(new String[]{DEFAULT_DATE_FORMAT});
+		ConvertUtils.register(dateConverter, java.util.Date.class);
+	}
+	
+	public ConvertUtilsBean getConvertUtilsBean() {
+		return convertUtilsBean;
+	}
+
+	public void setConvertUtilsBean(ConvertUtilsBean convertUtilsBean) {
+		this.convertUtilsBean = convertUtilsBean;
+	}
+
+	
 
 	
 }
