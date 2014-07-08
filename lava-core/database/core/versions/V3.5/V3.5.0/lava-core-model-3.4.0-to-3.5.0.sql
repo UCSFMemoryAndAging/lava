0fb381c9c848b11ad82768399380898ceebd86a5
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-07-08 15:04:05 -0700

    added properties to import_definition
    created import_definition_mapping_file to facilitate subclassing
    LavaFile
    
    modified import_log to store data_file_id, not name
    IN PROGRESS: need to add new counter columns
    
    import_log_message

--- lava-core/database/core/versions/V3.5/V3.5.0/lava-core-model-3.4.0-to-3.5.0.sql
+++ lava-core/database/core/versions/V3.5/V3.5.0/lava-core-model-3.4.0-to-3.5.0.sql
@@ -5,24 +5,56 @@ CREATE TABLE `import_definition` (
   `name` varchar(30) NOT NULL,
   `category` varchar(30) DEFAULT NULL,
   `mapping_file_id` int DEFAULT NULL,
+  `data_file_format` varchar(25) NULL,
+  `date_format` varchar(15) NULL,
+  `time_format` varchar(15) NULL,
   `notes` varchar(500) DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `import_definition_name_unique` (`name`),
   PRIMARY KEY (`definition_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `import_definition_mapping_file`;
+
+CREATE TABLE `import_definition_mapping_file` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `import_definition_mapping_file__id` FOREIGN KEY (`id`) REFERENCES `lava_file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `import_log`;
 
 CREATE TABLE `import_log` (
   `log_id` int NOT NULL AUTO_INCREMENT,
-  `filename` varchar(100) NOT NULL,
-  `definition_name` varchar(30) NULL,
   `import_timestamp` datetime NOT NULL,
   `imported_by` varchar(25) NOT NULL,
+  `data_file_id` int DEFAULT NULL,
+  `definition_name` varchar(30) NULL,
+  `total_records` smallint NULL,
+  `imported` smallint NULL,
+  `already_exist` smallint NULL,
+  `errors` smallint NULL,
+  `warnings` smallint NULL,
   `notes` varchar(500) DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`log_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `import_log_message`;
+
+CREATE TABLE `import_log_messsage` (
+  `message_id` int NOT NULL AUTO_INCREMENT,
+  `log_id` int NOT NULL,
+  `list_index` int NOT NULL,
+  `msg_type` varchar(10) NOT NULL,
+  `line_num` int NULL,
+  `message` varchar(255) NOT NULL,
+  PRIMARY KEY (`message_id`),
+  KEY `fk_import_log_message__log_id` (`log_id`),
+  CONSTRAINT `fk_import_log_message__log_id` FOREIGN KEY (`log_id`) REFERENCES `import_log` (`log_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- WAIT ON THIS UNTIL TIME TO RELEASE
 -- insert into versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
