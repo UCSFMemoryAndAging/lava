26e53ee27728bb128f9b4094f92a6748b3d19e32
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-07-08 15:52:57 -0700

    modified to support the lists with values used for required fields in creation of new entities
    also the *ExistRule properties are required

--- lava-crms/src/edu/ucsf/lava/crms/importer/controller/CrmsImportDefinitionHandler.java
+++ lava-crms/src/edu/ucsf/lava/crms/importer/controller/CrmsImportDefinitionHandler.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
 import org.springframework.webflow.definition.StateDefinition;
@@ -12,10 +13,12 @@ import org.springframework.webflow.execution.RequestContext;
 
 import edu.ucsf.lava.core.controller.ComponentCommand;
 import edu.ucsf.lava.core.importer.controller.ImportDefinitionHandler;
+import edu.ucsf.lava.core.manager.Managers;
 import edu.ucsf.lava.core.session.CoreSessionUtils;
 import edu.ucsf.lava.crms.auth.CrmsAuthUtils;
 import edu.ucsf.lava.crms.importer.model.CrmsImportDefinition;
-import edu.ucsf.lava.crms.importer.model.CrmsImportSetup;
+import edu.ucsf.lava.crms.manager.CrmsManagerUtils;
+import edu.ucsf.lava.crms.project.ProjectManager;
 import edu.ucsf.lava.crms.session.CrmsSessionUtils;
 
 
@@ -28,10 +31,16 @@ import edu.ucsf.lava.crms.session.CrmsSessionUtils;
  *
  */
 public class CrmsImportDefinitionHandler extends ImportDefinitionHandler {
+//	protected InstrumentManager instrumentManager;
+//	protected EnrollmentManager enrollmentManager;
+//	protected VisitManager visitManager; 
+	protected ProjectManager projectManager; 
 
 	public CrmsImportDefinitionHandler() {
 		super();
 		setHandledEntity("importDefinition", CrmsImportDefinition.class);
+		this.setRequiredFields(StringUtils.mergeStringArrays(this.getRequiredFields(), 
+				new String[]{"patientExistRule", "esExistRule", "visitExistRule", "instrExistRule"}));
 	}
 	
 	/**
@@ -44,16 +53,28 @@ public class CrmsImportDefinitionHandler extends ImportDefinitionHandler {
 	public Event preSetupFlowDirector(RequestContext context) throws Exception {
 		return new Event(this,CONTINUE_FLOW_EVENT_ID);
 	}
+
+	public void updateManagers(Managers managers){
+		super.updateManagers(managers);
+//		this.enrollmentManager = CrmsManagerUtils.getEnrollmentManager(managers);
+//		this.instrumentManager = CrmsManagerUtils.getInstrumentManager(managers);
+		this.projectManager = CrmsManagerUtils.getProjectManager(managers);
+//		this.visitManager = CrmsManagerUtils.getVisitManager(managers);
+	}
 	
 	@Override
 	public Map addReferenceData(RequestContext context, Object command,	BindingResult errors, Map model) {
 		HttpServletRequest request =  ((ServletExternalContext)context.getExternalContext()).getRequest();
-		//	load up dynamic lists
+		CrmsImportDefinition crmsImportDefinition = (CrmsImportDefinition) ((ComponentCommand)command).getComponents().get(this.getDefaultObjectName());
 	 	StateDefinition state = context.getCurrentState();
+
+		//	load up dynamic lists
+	 	String projName = crmsImportDefinition.getProjNameForContext(); 
+	 	
 		model = super.addReferenceData(context, command, errors, model); 
 		Map<String,Map<String,String>> dynamicLists = getDynamicLists(model);
-		CrmsImportDefinition crmsImportDefinition = (CrmsImportDefinition) ((ComponentCommand)command).getComponents().get(this.getDefaultObjectName());
-	 	if (state.getId().equals("edit")) {
+	 	if (state.getId().equals("add") || state.getId().equals("edit")) {
+	 		// context.projectList
 			// note that this list is filtered via projectAuth filter. CrmsAuthUser getAuthDaoFilters determines the projects to
 			// which a user has some kind of access. However, the list must be further filtered based on permissions to make sure 
 			// the user has the import permission for each project in the list.
@@ -61,8 +82,36 @@ public class CrmsImportDefinitionHandler extends ImportDefinitionHandler {
 			projList = CrmsAuthUtils.filterProjectListByPermission(CrmsSessionUtils.getCrmsCurrentUser(sessionManager,request),
 					CoreSessionUtils.getCurrentAction(sessionManager,request), projList);
 			dynamicLists.put("context.projectList", projList);
+
+			if (projName != null) {
+				// visit.visitTypes
+				dynamicLists.put("visit.visitTypes", listManager.getDynamicList("visit.visitTypes", 
+						"projectName", projName, String.class));
+				// visit.visitLocations
+				dynamicLists.put("visit.visitLocations", listManager.getDynamicList("visit.visitLocations", 
+						"projectName", projName, String.class));
+				// visit.visitWith
+				dynamicLists.put("project.staffList", listManager.getDynamicList("project.projectStaffList", 
+						"projectName", projName, String.class));
+			
+				// enrollmentStatus.projectStatus
+				//try for project status based on full projName
+				dynamicLists.put("enrollmentStatus.projectStatus",listManager.getDynamicList("enrollmentStatus.projectStatus", 
+						"projectName", projName, String.class));
+				//if no entries found (size is 1 because "","" entry added to all dynamic lists), then use just project part as the project name leaving off unit 
+				if (dynamicLists.get("enrollmentStatus.projectStatus").size()==1){
+					String project = projectManager.getProject(projName).getProject();
+					dynamicLists.put("enrollmentStatus.projectStatus",listManager.getDynamicList("enrollmentStatus.projectStatus", 
+						"projectName", project, String.class));
+				}
+				//if no entries found based on projName or just project without the unit then get general statuses
+				if (dynamicLists.get("enrollmentStatus.projectStatus").size()==1){
+					dynamicLists.put("enrollmentStatus.projectStatus",listManager.getDynamicList("enrollmentStatus.projectStatus", 
+							"projectName", "GENERAL", String.class));
+				}
+			}
+			model.put("dynamicLists", dynamicLists);
 		}
-		model.put("dynamicLists", dynamicLists);
 		return model; 
 	}
 
