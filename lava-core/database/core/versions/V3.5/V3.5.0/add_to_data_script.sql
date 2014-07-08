ddb822a9e37426865964be017bc53b7c8cf194f1
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-07-08 14:57:03 -0700

    temporary script

--- lava-core/database/core/versions/V3.5/V3.5.0/add_to_data_script.sql
+++ lava-core/database/core/versions/V3.5/V3.5.0/add_to_data_script.sql
@@ -1,4 +1,6 @@
 -- make re-runnable
+-- this will be part of a new lava-core-data-3.5.0.sql script, as well as an update lava-core
+-- to 3.5.0 update script
 
 update viewproperty set label = 'Content Type' where entity = 'lavaFile' and property = 'contentType';
 update viewproperty set context = 'c' where entity = 'lavaFile' and property = 'name';
