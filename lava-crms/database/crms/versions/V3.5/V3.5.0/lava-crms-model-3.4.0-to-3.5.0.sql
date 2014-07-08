2299e5ae43d8587fe9332356458a2e31ad263c35
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-07-08 15:43:55 -0700

    crms_import_defintion additions
    PENDING: crms_import_log modifications (adding count columns)

--- lava-crms/database/crms/versions/V3.5/V3.5.0/lava-crms-model-3.4.0-to-3.5.0.sql
+++ lava-crms/database/crms/versions/V3.5/V3.5.0/lava-crms-model-3.4.0-to-3.5.0.sql
@@ -2,18 +2,23 @@ DROP TABLE IF EXISTS `crms_import_definition`;
 
 CREATE TABLE `crms_import_definition` (
   `definition_id` int NOT NULL,
-  `patient_must_exist` boolean NULL,
-  `visit_must_exist` boolean NULL,
-  `visit_type_supplied` boolean NULL,
+  `patient_exist_rule` smallint NULL,
+  `patient_update` boolean NULL,
+  `es_exist_rule` smallint NULL,
+  `es_update` boolean NULL,
+  `es_status_supplied` boolean NULL,
+  `es_status` varchar(25) NULL,
+  `visit_exist_rule` smallint NULL,
+  `visit_update` boolean NULL,
   `visit_type` varchar(25) NULL,
-  `visit_loc_supplied` boolean NULL,
   `visit_loc` varchar(25) NULL,
-  `visit_with_supplied` boolean NULL,
   `visit_with` varchar(25) NULL,
-  `visit_status_supplied` boolean NULL,
   `visit_status` varchar(25) NULL,
-  `instr_must_exist` boolean NULL,
+  `instr_exist_rule` smallint NULL,
+  `instr_update` boolean NULL,
   `instr_type` varchar(25) NULL,
+  `instr_ver` varchar(25) NULL,
+  `instr_dc_status` varchar(25) NULL,
   PRIMARY KEY (`definition_id`),
   CONSTRAINT `crms_import_definition__definition_id` FOREIGN KEY (`definition_id`) REFERENCES `import_definition` (`definition_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -30,4 +35,3 @@ CREATE TABLE `crms_import_log` (
 -- WAIT ON THIS UNTIL READY FOR RELEASE
 -- insert into versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
 -- 	VALUES ('lava-crms-model','3.5.0',NOW(),3,5,0,0);
-
