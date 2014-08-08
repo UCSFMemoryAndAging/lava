0236bb4cf5c2daac404c0820a044070f274be196
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-08-08 16:40:28 -0700

    fixed up version history values

--- lava-crms/database/crms/versions/V3.4/V3.4.1/update/lava-crms-model-3.4.0-to-3.4.1.sql
+++ lava-crms/database/crms/versions/V3.4/V3.4.1/update/lava-crms-model-3.4.0-to-3.4.1.sql
@@ -21,8 +21,8 @@ ALTER TABLE `enrollmentstatus` CHANGE COLUMN `SubjectStudyID` `SubjectStudyID` V
 ALTER TABLE `projectunit` ADD COLUMN `ProjCode` VARCHAR(4) NULL DEFAULT NULL AFTER `Project`;
 ALTER TABLE `projectunit` ADD COLUMN `UnitCode` VARCHAR(4) NULL DEFAULT NULL AFTER `Unit`;
 
-DELETE FROM versionhistory WHERE module='lava-crms-model' AND version='3.4.2';
+DELETE FROM versionhistory WHERE module='lava-crms-model' AND version='3.4.1';
 INSERT INTO versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
-	VALUES ('lava-crms-model','3.4.2',NOW(),3,4,2,0);
+	VALUES ('lava-crms-model','3.4.1',NOW(),3,4,1,0);
 
 
