ec1a157af80b6728801e8bf00dd1a737651c2562
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-08-08 16:23:02 -0700

    directory path and file naming conventions for storing import mapping and data files in the repository

--- lava-core/src/edu/ucsf/lava/core/file/ImportRepositoryStrategy.java
+++ lava-core/src/edu/ucsf/lava/core/file/ImportRepositoryStrategy.java
@@ -1,11 +1,37 @@
 package edu.ucsf.lava.core.file;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import edu.ucsf.lava.core.file.ArchivingLocalFileSystemRepositoryStrategy;
 import edu.ucsf.lava.core.file.exception.FileAccessException;
-import edu.ucsf.lava.core.file.model.ImportDefinitionMappingFile;
+import edu.ucsf.lava.core.file.model.ImportFile;
 import edu.ucsf.lava.core.file.model.LavaFile;
+
+/**
+ * Applications will typically want to configure application specific Spring beans for
+ * repository and respositoryStrategy to define rootPath and archiveRoot properties 
+ * specific to the application. These beans are already defined in lava-core core-import.xml so
+ * those definitions can be referenced by the application definitions.
+ * 
+ * The repository bean can use the lava-core Repository class (e.g. BaseFileRepository) by designating
+ * the "importLavaFileRepository" bean as a parent and only defining the "strategy" property to be
+ * the application defined strategy bean.
+ * 
+ * The strategy bean can use this class, ImportRepositoryStrategy, by designating "importRepositoryStrategy"
+ * bean as a parent and defining the "rootPath" and "archiveRoot" properties.
+ * 
+ * Typically the only need to subclass this class in the application would be if the application
+ * had a different file naming and directory path naming setup. generateLocation would be overridden to
+ * define a new directory path naming system, and generateFileId would be overridden to define
+ * a new file naming system.
+ * 
+ * @author ctoohey
+ *
+ */
 
 public class ImportRepositoryStrategy extends ArchivingLocalFileSystemRepositoryStrategy {
 	public static String IMPORT_REPOSITORY_ID = "import_repository";
@@ -33,22 +59,25 @@ public class ImportRepositoryStrategy extends ArchivingLocalFileSystemRepository
 	
 	
 	public String generateFileId(LavaFile lavaFile) throws FileAccessException {
+		// import definition mapping files can just retain their name since there should only be one
+		// instance of such a file (there may be multiple edits of the file over time in which case
+		// the file is replaced in the definition and the prior versions are archived)
 		
-		// for import data files and import config mapping files the fileId can just be
-		// the filename of the file, because no PHI in filename so do not need to hide it in 
-		// the file system
-		return lavaFile.getName();
+		// in contrast, import data files may have the same name but different data (or incremental
+		// data additions), so these files should have unique names, so append a timestamp after
+		// their name
+		StringBuffer filename = new StringBuffer(lavaFile.getName());
+		if (lavaFile.getContentType().equals(IMPORT_DATA_FILE_TYPE)) {
+			DateFormat formatter = new SimpleDateFormat("yyyymmdd_HHmmss_SSS", Locale.getDefault());
+			filename.append("_").append(formatter.format(new Date()));
+		}
+		return filename.toString();
 	}
 	
 	public String generateLocation(LavaFile lavaFile) throws FileAccessException {
-		StringBuffer location = new StringBuffer();
-		if (lavaFile.getContentType().equals(IMPORT_DATA_FILE_TYPE)) {
-			location.append("data");
-		}
-		else if (lavaFile.getContentType().equals(IMPORT_DEF_MAPPING_FILE_TYPE)) {
-			String definitionNameEncoded = ((ImportDefinitionMappingFile)lavaFile).getDefinitionName().replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-			location.append("mapping").append(File.separatorChar).append(definitionNameEncoded);
-		}
+		String definitionNameEncoded = ((ImportFile)lavaFile).getDefinitionName().replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+		StringBuffer location = new StringBuffer(definitionNameEncoded).append(File.separatorChar);
+		// mapping files and data files go directly under a folder named after the definition
 		location.append(File.separatorChar);
 		return location.toString();
 	}
