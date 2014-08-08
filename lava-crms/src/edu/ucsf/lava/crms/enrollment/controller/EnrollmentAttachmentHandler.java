9430265f684f306d209c4569cb90727d8054eadd
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-08-08 16:36:38 -0700

    add flag to model to signal to view to support view logic

--- lava-crms/src/edu/ucsf/lava/crms/enrollment/controller/EnrollmentAttachmentHandler.java
+++ lava-crms/src/edu/ucsf/lava/crms/enrollment/controller/EnrollmentAttachmentHandler.java
@@ -1,5 +1,13 @@
 package edu.ucsf.lava.crms.enrollment.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.execution.RequestContext;
+
 import edu.ucsf.lava.crms.file.controller.BaseCrmsFileHandler;
 import edu.ucsf.lava.crms.file.model.CrmsFile;
 import edu.ucsf.lava.crms.session.CrmsSessionUtils;
@@ -12,5 +20,14 @@ public class EnrollmentAttachmentHandler extends BaseCrmsFileHandler {
 		this.setHandledEntity("enrollmentAttachment", CrmsFile.class);
 	}
 
-	
+	public Map addReferenceData(RequestContext context, Object command, BindingResult errors, Map model)
+	{
+		HttpServletRequest request =  ((ServletExternalContext)context.getExternalContext()).getRequest();
+		// cannot get request parameter value directly from the request because of post-redirect-get second request. instead. the
+		// flow will pass request parameters with predefined names ("param", "param1", etc.) into flowScope to be accessed
+		String parentView = context.getFlowScope().getString("param");
+		model.put("parentView", parentView);
+		return super.addReferenceData(context, command, errors, model);
+	}
+
 }
