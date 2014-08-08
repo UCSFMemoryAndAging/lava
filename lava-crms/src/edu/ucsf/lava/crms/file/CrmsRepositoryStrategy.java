c9aea6e84e4063505afcba99cf107617ddf83a9f
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-08-08 16:37:21 -0700

    came up with default generateLocation strategy which works in conjunction with lava-core strategy

--- lava-crms/src/edu/ucsf/lava/crms/file/CrmsRepositoryStrategy.java
+++ lava-crms/src/edu/ucsf/lava/crms/file/CrmsRepositoryStrategy.java
@@ -1,10 +1,34 @@
 package edu.ucsf.lava.crms.file;
 
+import java.io.File;
+
+/**
+ * Applications will typically want to configure application specific Spring beans for
+ * repository and respositoryStrategy to define rootPath and archiveRoot properties 
+ * specific to the application. These beans are already defined in lava-crms crms-file.xml so
+ * those definitions can be referenced by the application definitions.
+ * 
+ * 
+ * The repository bean can use a lava-core Repository class (e.g. BaseFileRepository) by designating
+ * the "crmsFileRepository" bean as a parent and only defining the "strategy" property to be
+ * the application defined strategy bean.
+ * 
+ * The strategy bean can use this class, CrmsRepositoryStrategy, by designating "crmsRepositoryStrategy"
+ * bean as a parent and defining the "rootPath" and "archiveRoot" properties.
+ * 
+ * Typically the only need to subclass this class in the application would be if the application
+ * had a different file naming and directory path naming setup. generateLocation would be overridden to
+ * define a new directory path naming system, and generateFileId would be overridden to define
+ * a new file naming system.
+ */
+
 import edu.ucsf.lava.core.file.ArchivingLocalFileSystemRepositoryStrategy;
+import edu.ucsf.lava.core.file.exception.FileAccessException;
 import edu.ucsf.lava.core.file.model.LavaFile;
 import edu.ucsf.lava.crms.file.model.CrmsFile;
 
 public class CrmsRepositoryStrategy extends ArchivingLocalFileSystemRepositoryStrategy {
+	public static String CRMS_REPOSITORY_ID = "lava_crms";
 
 	public CrmsRepositoryStrategy() {
 		super();
@@ -15,13 +39,9 @@ public class CrmsRepositoryStrategy extends ArchivingLocalFileSystemRepositorySt
 	 * getRepository(file) to obtain the repository for a given file, FileRepositories
 	 * iterates thru all of the repositories calling handlesFile on each one to 
 	 * determine which repository to use for the file operation.
-	 * 
-	 * Here, handle all CrmsFiles. Could instead subclass this strategy class and use other criteria such 
-	 * as testing against file repositoryId (which would have to be assigned on CrmsFile instantiation) or
-	 * test against file contentType.
 	 **/ 
 	public boolean handlesFile(LavaFile file) {
-		if (file instanceof CrmsFile) {
+		if (file.getRepositoryId().equals(CRMS_REPOSITORY_ID)) {
 			return true;
 		}
 		else {
@@ -30,22 +50,32 @@ public class CrmsRepositoryStrategy extends ArchivingLocalFileSystemRepositorySt
 	}
 	
 	/**
-	 * TODO: subclass this class with an application level repository strategy and override generateLocation 
-	 * to create an organizational directory structure for the repository (in conjunction with the Spring bean
-	 * settings for the Strategy rootPath and archiveRoot), and override generateFileId for file naming.
-	 * 
-	 * The implementation of generateLocation and generateFileId in LocalFileSystemRepositoryStrategy may 
-	 * suffice, however may want to organize directories by module ("patient", "enrollment", "visit" and 
-	 * "instrument") and below that a directory named after PIDN, to which uploaded files are written. Repo 
-	 * files could have the same name they have in the user's file system. But e.g. if the filenames 
-	 * themselves contain PHI, could go with LocalFileSystemRepository generateFileId method which generates 
-	 * filenames.  
-	 * 
-	 * At a minimum, will want to define repository and strategy beans in the application using existing
-	 * classes (e.g. BaseFileRepository and ArchivingLocalFileSystemRepositoryStrategy, respectively) to
-	 * set the rootPath and archiveRoot properties on the strategy bean based on where files should be stored 
-	 * in your system.
+	 * Utilize the lava-core LocalFileSystemRepositoryStrategy generateLocation, but
+	 * prepend with a folder indicating the type of entity under which files of that
+	 * type are stored.
 	 */
 
+	public String generateLocation(LavaFile lavaFile) throws FileAccessException {
+		StringBuffer path = new StringBuffer();
+		String subLocation = super.generateLocation(lavaFile);
+		CrmsFile crmsFile = (CrmsFile) lavaFile;
+		if (crmsFile.getInstrId() != null) {
+			path.append("instr").append(File.separatorChar);
+		}
+		else if (crmsFile.getVisitId() != null) {
+			path.append("visit").append(File.separatorChar);
+		}
+		else if (crmsFile.getEnrollStatId() != null) {
+			path.append("enroll").append(File.separatorChar);
+		}
+		else if (crmsFile.getConsentId() != null) {
+			path.append("consent").append(File.separatorChar);
+		}
+		else if (crmsFile.getPidn() != null) {
+			path.append("patient").append(File.separatorChar);
+		}
+		path.append(subLocation);
+		return path.toString();
+	}
 	
 }
