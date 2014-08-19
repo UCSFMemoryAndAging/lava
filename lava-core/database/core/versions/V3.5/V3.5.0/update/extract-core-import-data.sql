afa167ac1e24dbf75ca21bd3013b86b4e4b40190
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-08-18 17:14:22 -0700

    deleted temporary scripts just for core import metadata

--- lava-core/database/core/versions/V3.5/V3.5.0/update/extract-core-import-data.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'DELETE from `viewproperty` where `instance`=''lava'' and `scope`=''core-import'';';
-SELECT 'DELETE from `hibernateproperty` where `instance`=''lava'' and `scope`=''core-import'';';  
-SELECT 'DELETE from `listvalues` where `instance`=''lava'' and `scope`=''core-import'';';  
-SELECT 'DELETE from `list` where `instance`=''lava'' and `scope`=''core-import'';';  
-call util_CreateMetadataInsertStatements('lava','core-import','%');
