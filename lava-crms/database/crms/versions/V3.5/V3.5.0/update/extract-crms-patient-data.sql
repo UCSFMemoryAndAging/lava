f8c1a09272ab27b070f612e2883fa4fe8f546638
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-08-19 17:23:36 -0700

    remove temporary scripts

--- lava-crms/database/crms/versions/V3.5/V3.5.0/update/extract-crms-patient-data.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'DELETE from `viewproperty` where `instance`=''lava'' and `scope`=''crms'' and `entity` = ''patient'';';
-SELECT 'DELETE from `hibernateproperty` where `instance`=''lava'' and `scope`=''crms'' and `entity` = ''patient'';';  
-SELECT 'DELETE from `listvalues` where `instance`=''lava'' and `scope`=''crms'' and `entity` = ''patient'';';  
-SELECT 'DELETE from `list` where `instance`=''lava'' and `scope`=''crms'' and `entity` = ''patient'';';  
-call util_CreateMetadataInsertStatements('lava','crms','patient');
