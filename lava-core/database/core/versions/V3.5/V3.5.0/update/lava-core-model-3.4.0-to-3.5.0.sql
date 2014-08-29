4a223b2f90188ac1ff0aeeeb6ab83cd2ca13c87b
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-08-29 14:08:55 -0700

    added missing create table for import_file
    added FK constraint from import_definition to import_file for mapping
    file
    
    import_log now stores definition_id, not definition_name
    FK constraint from import_log to import_definition
    FK constraint from import_log to import_file

--- lava-core/database/core/versions/V3.5/V3.5.0/update/lava-core-model-3.4.0-to-3.5.0.sql
+++ lava-core/database/core/versions/V3.5/V3.5.0/update/lava-core-model-3.4.0-to-3.5.0.sql
@@ -1,3 +1,18 @@
+DROP TABLE IF EXISTS `import_file`;
+
+-- this table exists purely to support a Java subclass of a base class that maps lava_file,
+-- where the subclass just has non-persistent properties, but must be mapped because of
+-- Hibernate requiring a mapping for a subclass of a persisted base class
+CREATE TABLE `import_file` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `import_file__id` FOREIGN KEY (`id`) REFERENCES `lava_file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION) 
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- note if dropping tables to start fresh need, due to FK constraints, need to drop in this order:
+-- import_log_message, then import_log, then import_definition
+-- also need to drop crms_import_definition, crms_import_log
 DROP TABLE IF EXISTS `import_definition`;
 
 CREATE TABLE `import_definition` (
@@ -12,15 +27,12 @@ CREATE TABLE `import_definition` (
   `notes` varchar(500) DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `import_definition_name_unique` (`name`),
-  PRIMARY KEY (`definition_id`)
+  PRIMARY KEY (`definition_id`),
+  KEY `fk_import_definition__mapping_file_id` (`mapping_file_id`),
+  CONSTRAINT `fk_import_definition__mapping_file_id` FOREIGN KEY (`mapping_file_id`) REFERENCES `import_file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `import_definition_mapping_file`;
 
-CREATE TABLE `import_definition_mapping_file` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `import_definition_mapping_file__id` FOREIGN KEY (`id`) REFERENCES `lava_file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `import_log`;
@@ -29,8 +41,8 @@ CREATE TABLE `import_log` (
   `log_id` int NOT NULL AUTO_INCREMENT,
   `import_timestamp` datetime NOT NULL,
   `imported_by` varchar(25) NOT NULL,
-  `data_file_id` int DEFAULT NULL,
-  `definition_name` varchar(30) NULL,
+  `data_file_id` int NOT NULL,
+  `definition_id` int NOT NULL,
   `total_records` int NULL,
   `imported` int NULL,
   `updated` int NULL,
@@ -39,8 +51,13 @@ CREATE TABLE `import_log` (
   `warnings` int NULL,
   `notes` varchar(500) DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`log_id`)
+  PRIMARY KEY (`log_id`),
+  KEY `fk_import_log__data_file_id` (`data_file_id`),
+  CONSTRAINT `fk_import_log__data_file_id` FOREIGN KEY (`data_file_id`) REFERENCES `import_file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  KEY `fk_import_log__definition_id` (`definition_id`),
+  CONSTRAINT `fk_import_log__definition_id` FOREIGN KEY (`definition_id`) REFERENCES `import_definition` (`definition_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 
 DROP TABLE IF EXISTS `import_log_message`;
@@ -56,6 +73,7 @@ CREATE TABLE `import_log_message` (
   KEY `fk_import_log_message__log_id` (`log_id`),
   CONSTRAINT `fk_import_log_message__log_id` FOREIGN KEY (`log_id`) REFERENCES `import_log` (`log_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 
 ALTER TABLE `lava_file` ADD COLUMN `notes` VARCHAR(255) DEFAULT NULL AFTER `checksum`;
