feb94f26f6c9612520e5683afc2559b40f5a3c72
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-11-12 03:07:31 -0800

    import_log_message message 2000 chars

--- lava-core/database/core/versions/V3.5/V3.5.1/update/lava-core-model-3.5.0-to-3.5.1.sql
+++ lava-core/database/core/versions/V3.5/V3.5.1/update/lava-core-model-3.5.0-to-3.5.1.sql
@@ -4,6 +4,9 @@ ALTER TABLE `import_definition` MODIFY `category` VARCHAR(50);
 -- add truncation to import_definition as a flag for how to handle truncation. either throw an error or truncate to the metadata length
 ALTER TABLE import_definition ADD COLUMN truncate SMALLINT NULL DEFAULT NULL AFTER time_format;
 
+-- import_log_message message needs to be much larger than 255 to accomodate exception msgs, cause, stack trace
+ALTER TABLE `import_log_message` MODIFY `message` VARCHAR(2000);
+
 DELETE FROM versionhistory WHERE module='lava-core-model' AND version='3.5.1';
 INSERT INTO versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
 VALUES ('lava-core-model','3.5.1','2014-11-05',3,5,1,0);
