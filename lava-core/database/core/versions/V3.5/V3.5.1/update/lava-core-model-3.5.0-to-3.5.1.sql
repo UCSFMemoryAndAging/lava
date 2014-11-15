f5d86351d0ff7af91e8cb85ccb904d82255df7d9
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-11-14 22:35:50 -0800

    import_log_message table message column size is now 5000 to accommodate truncation warning messages with very large text fields (warning message contains the original untruncated text)

--- lava-core/database/core/versions/V3.5/V3.5.1/update/lava-core-model-3.5.0-to-3.5.1.sql
+++ lava-core/database/core/versions/V3.5/V3.5.1/update/lava-core-model-3.5.0-to-3.5.1.sql
@@ -5,7 +5,7 @@ ALTER TABLE `import_definition` MODIFY `category` VARCHAR(50);
 ALTER TABLE import_definition ADD COLUMN truncate SMALLINT NULL DEFAULT NULL AFTER time_format;
 
 -- import_log_message message needs to be much larger than 255 to accomodate exception msgs, cause, stack trace
-ALTER TABLE `import_log_message` MODIFY `message` VARCHAR(2000);
+ALTER TABLE `import_log_message` MODIFY `message` VARCHAR(5000);
 
 DELETE FROM versionhistory WHERE module='lava-core-model' AND version='3.5.1';
 INSERT INTO versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
