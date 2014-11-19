c6c08ef2016fafd90c088d72fe0f96f493fa45b6
Charlie Toohey <ctoohey@memory.ucsf.edu>
2014-11-19 10:13:43 -0800

    org imports

--- lava-core/src/edu/ucsf/lava/core/importer/controller/AllImportDefinitionsHandler.java
+++ lava-core/src/edu/ucsf/lava/core/importer/controller/AllImportDefinitionsHandler.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.webflow.context.servlet.ServletExternalContext;
 import org.springframework.webflow.execution.RequestContext;
 
-import edu.ucsf.lava.core.auth.model.AuthRole;
 import edu.ucsf.lava.core.controller.BaseListComponentHandler;
 import edu.ucsf.lava.core.dao.LavaDaoFilter;
 import edu.ucsf.lava.core.importer.model.ImportDefinition;
