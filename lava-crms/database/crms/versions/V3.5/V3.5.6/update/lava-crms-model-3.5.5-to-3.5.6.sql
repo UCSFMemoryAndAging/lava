5457f0ffe8ab6aae757a7b5069fda7b7d531f838
ctoohey <charlie.toohey@ucsf.edu>
2016-05-20 09:26:38 -0700

    Modify some import labels
    Expanded import definition instrument properties to accommodate 15
    instruments, up from 10
    
    Add instrMappingAlias properties for supporting multiple instances of
    the same instrument in an import record
    
    Add instrCalculate properties as a flag to indicate whether the import
    should run instrument calculations after data is imported or updated

---
+++ lava-crms/database/crms/versions/V3.5/V3.5.6/update/lava-crms-model-3.5.5-to-3.5.6.sql
@@ -0,0 +1,59 @@
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias varchar(25) NULL DEFAULT NULL AFTER `instr_ver`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias2 varchar(25) NULL DEFAULT NULL AFTER `instr_ver2`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias3 varchar(25) NULL DEFAULT NULL AFTER `instr_ver3`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias4 varchar(25) NULL DEFAULT NULL AFTER `instr_ver4`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias5 varchar(25) NULL DEFAULT NULL AFTER `instr_ver5`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias6 varchar(25) NULL DEFAULT NULL AFTER `instr_ver6`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias7 varchar(25) NULL DEFAULT NULL AFTER `instr_ver7`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias8 varchar(25) NULL DEFAULT NULL AFTER `instr_ver8`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias9 varchar(25) NULL DEFAULT NULL AFTER `instr_ver9`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias10 varchar(25) NULL DEFAULT NULL AFTER `instr_ver10`;
+
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate2 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver2;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate3 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver3;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate4 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver4;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate5 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver5;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate6 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver6;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate7 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver7;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate8 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver8;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate9 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver9;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate10 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver10;
+
+ALTER TABLE crms_import_definition ADD COLUMN instr_type11 varchar(25) NULL DEFAULT NULL AFTER `instr_calculate10`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_ver11 varchar(25) NULL DEFAULT NULL AFTER `instr_type11`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias11 varchar(25) NULL DEFAULT NULL AFTER `instr_ver11`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_caregiver11 BOOLEAN NULL DEFAULT NULL AFTER instr_mapping_alias11;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate11 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver11;
+
+ALTER TABLE crms_import_definition ADD COLUMN instr_type12 varchar(25) NULL DEFAULT NULL AFTER `instr_calculate11`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_ver12 varchar(25) NULL DEFAULT NULL AFTER `instr_type12`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias12 varchar(25) NULL DEFAULT NULL AFTER `instr_ver12`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_caregiver12 BOOLEAN NULL DEFAULT NULL AFTER instr_mapping_alias12;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate12 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver12;
+
+ALTER TABLE crms_import_definition ADD COLUMN instr_type13 varchar(25) NULL DEFAULT NULL AFTER `instr_calculate12`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_ver13 varchar(25) NULL DEFAULT NULL AFTER `instr_type13`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias13 varchar(25) NULL DEFAULT NULL AFTER `instr_ver13`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_caregiver13 BOOLEAN NULL DEFAULT NULL AFTER instr_mapping_alias13;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate13 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver13;
+
+ALTER TABLE crms_import_definition ADD COLUMN instr_type14 varchar(25) NULL DEFAULT NULL AFTER `instr_calculate13`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_ver14 varchar(25) NULL DEFAULT NULL AFTER `instr_type14`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias14 varchar(25) NULL DEFAULT NULL AFTER `instr_ver14`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_caregiver14 BOOLEAN NULL DEFAULT NULL AFTER instr_mapping_alias14;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate14 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver14;
+
+ALTER TABLE crms_import_definition ADD COLUMN instr_type15 varchar(25) NULL DEFAULT NULL AFTER `instr_calculate14`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_ver15 varchar(25) NULL DEFAULT NULL AFTER `instr_type15`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_mapping_alias15 varchar(25) NULL DEFAULT NULL AFTER `instr_ver15`;
+ALTER TABLE crms_import_definition ADD COLUMN instr_caregiver15 BOOLEAN NULL DEFAULT NULL AFTER instr_mapping_alias15;
+ALTER TABLE crms_import_definition ADD COLUMN instr_calculate15 BOOLEAN NULL DEFAULT NULL AFTER instr_caregiver15;
+
+
+
+DELETE FROM versionhistory WHERE module='lava-crms-model' AND version='3.5.6';
+INSERT INTO versionhistory(`Module`,`Version`,`VersionDate`,`Major`,`Minor`,`Fix`,`UpdateRequired`)
+VALUES ('lava-crms-model','3.5.6','2016-05-20',3,5,6,0);
+
+
